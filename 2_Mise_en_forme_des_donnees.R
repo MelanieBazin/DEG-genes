e631c193a18e0decb0df8ba91cc9e81700740fbc
MelanieBazin <melanie19.bazin@gmail.com>
2021-04-23 19:22:47 +0200

    Ajout des condition d'ouverture des fichier pour les condition PGM et KU80c

--- 2_Mise_en_forme_des_donnees.R
+++ 2_Mise_en_forme_des_donnees.R
@@ -8,26 +8,17 @@ condition = "CTIP"
 
 #### Ouverture des fichiers ####
 files = list.files(path = paste0("./DATA/",type))
-files = files[grep(condition,files)]
 
-control = read.table(paste0("./DATA/", type,"/",files[grep("CTRL",files)]),h = T,sep = "\t")
-rnai = read.table(paste0("./DATA/", type,"/",files[setdiff(1:length(files),grep("CTRL",files))]),h = T,sep = "\t")
-
-####### Sélection de gènes #####
-selection = c("Ku","ku","PGM","NOWA","PTIWI","mt","TFIIS4","Spo11","Mre11","CER","Rad51", "Lig", "EZL", "SPT", "DCL", "CtIP", "XRCC4", "PDSG2", "PolX", "CAF1")
-
-selection_ID =c()
-
-for(i in selection){
-  selection_ID = c(selection_ID,annotation$ID[grep(i,annotation$SYNONYMS)])
-  
-}
-control = control[is.element(control$ID, selection_ID),]
-rnai = rnai[is.element(rnai$ID, selection_ID),]
 
 #### Definition des paramètres selon l'ARNi étudier ########
 
 if (condition == "CTIP") {
+  #Ouverture des fichiers
+  files = files[grep(condition,files)]
+  control = read.table(paste0("./DATA/", type,"/",files[grep("CTRL",files)]),h = T,sep = "\t")
+  rnai = read.table(paste0("./DATA/", type,"/",files[setdiff(1:length(files),grep("CTRL",files))]),h = T,sep = "\t")
+  
+  
   # Sélectionner les points à regrouper comme faisant parti du même timing
   timing_ctrl = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",2))
   timing_rnai = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",1))
@@ -41,6 +32,12 @@ if (condition == "CTIP") {
   pvalue = 0.2
   
 }else if (condition == "XRCC4"){
+  #Ouverture des fichiers
+  files = files[grep(condition,files)]
+  control = read.table(paste0("./DATA/", type,"/",files[grep("CTRL",files)]),h = T,sep = "\t")
+  rnai = read.table(paste0("./DATA/", type,"/",files[setdiff(1:length(files),grep("CTRL",files))]),h = T,sep = "\t")
+  
+  
   # Sélectionner les points à regrouper comme faisant parti du même timing
   timing_ctrl = c(rep("VEG",1),rep("EARLY",1),rep("INTER",1),rep("LATE",2))
   timing_rnai = timing_ctrl
@@ -50,9 +47,62 @@ if (condition == "CTIP") {
   rnai = rnai[,c(1,6,2:5)]
   
   # Definiton des variables DESeq2
-  FC = 2
+  FC = 1.1
+  pvalue = 0.2
+}else if (condition == "PGM"){
+  #Ouverture des fichiers
+  control_nd7 = read.table(paste0("./DATA/", type,"/",files[grep("ND7",files)]),h = T,sep = "\t")
+  control_icl7 = read.table(paste0("./DATA/", type,"/",files[grep("ICL7",files)]),h = T,sep = "\t")
+  rnai = read.table(paste0("./DATA/", type,"/",files[grep(condition,files)]),h = T,sep = "\t")
+  
+  # Réordonner les colonne du tableau pour les mettre dans l'ordre de la cinétique
+  control_nd7 = control_nd7[,c(1,8,2:7)]
+  control_icl7 = control_icl7[,c(1,8,2:7)]
+  rnai = rnai[,c(1,8,2:7)]
+  
+  # Sélectionner les points à regrouper comme faisant parti du même timing
+  timing_ctrl_nd7 = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
+  timing_ctrl_icl7 = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
+  timing_rnai = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
+
+  # Definiton des variables DESeq2
+  FC = 1.1
+  pvalue = 0.2
+  
+  
+}else if (condition == "KU80c"){
+  #Ouverture des fichiers
+  control_nd7 = read.table(paste0("./DATA/", type,"/",files[grep("ND7",files)]),h = T,sep = "\t")
+  control_icl7 = read.table(paste0("./DATA/", type,"/",files[grep("ICL7",files)]),h = T,sep = "\t")
+  rnai = read.table(paste0("./DATA/", type,"/",files[grep(condition,files)]),h = T,sep = "\t")
+  
+  # Réordonner les colonne du tableau pour les mettre dans l'ordre de la cinétique
+  control_nd7 = control_nd7[,c(1,8,2:7)]
+  control_icl7 = control_icl7[,c(1,8,2:7)]
+  rnai = rnai[,c(1,8,2:7)]
+  
+  # Sélectionner les points à regrouper comme faisant parti du même timing
+  timing_ctrl_nd7 = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
+  timing_ctrl_icl7 = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
+  timing_rnai = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
+
+  
+  # Definiton des variables DESeq2
+  FC = 1.1
   pvalue = 0.2
 }
+
+####### Sélection de gènes #####
+selection = c("Ku","ku","PGM","NOWA","PTIWI","mt","TFIIS4","Spo11","Mre11","CER","Rad51", "Lig", "EZL", "SPT", "DCL", "CtIP", "XRCC4", "PDSG2", "PolX", "CAF1")
+
+selection_ID =c()
+
+for(i in selection){
+  selection_ID = c(selection_ID,annotation$ID[grep(i,annotation$SYNONYMS)])
+  
+}
+control = control[is.element(control$ID, selection_ID),]
+rnai = rnai[is.element(rnai$ID, selection_ID),]
 
 ####### Mise en forme des données pour DESeq2 ##############
 # Fusion des tableau de comptage controle et ARNi

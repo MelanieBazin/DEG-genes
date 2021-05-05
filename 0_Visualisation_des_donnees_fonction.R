7f95c0e8076ca652a51462b9e9417b76e0f7c666
MelanieBazin <melanie19.bazin@gmail.com>
2021-05-05 13:54:38 +0200

    correctiond des beugues pour que le code soit fonctionelle, intégartion directe d'un certain nombre de fonction direteemnt dans la fonction custering

--- 0_Visualisation_des_donnees_fonction.R
+++ 0_Visualisation_des_donnees_fonction.R
@@ -110,30 +110,31 @@ library("factoextra")
 library(ggplot2)
 library(gtools)
 
-PCA_plot_generator <- function(Expression_Mat, colors,save_path,max_dim=3,barplot_max_dim=3,image_prefix="PCA_",show_barplot=T, vline=0, ...) {
+PCA_plot_generator <- function(Expression_Mat, colors,save_path, main,max_dim=3,barplot_max_dim=3,image_prefix="PCA_",show_barplot=T, vline=0, ...) {
   resExp = PCA(t(Expression_Mat), graph = F)
+  
   if(show_barplot) {
     eigenvalues <- resExp$eig
     pdf(paste0(save_path,image_prefix,"_PCA_Variance.pdf"))
-    barplot(eigenvalues[1:barplot_max_dim, 2], names.arg=1:barplot_max_dim, 
-            main = "Variances",
-            xlab = "Principal Components",
-            ylab = "Percentage of variances",
-            col ="gray", ...)
-    if(vline!=0) {
-      abline(v=vline,lty=2,lwd=2)
-    }
+      barplot(eigenvalues[1:barplot_max_dim, 2], names.arg=1:barplot_max_dim, 
+              main = "Variances",
+              xlab = "Principal Components",
+              ylab = "Percentage of variances",
+              col ="gray")
+      if(vline!=0) {
+        abline(v=vline,lty=2,lwd=2)
+      }
     dev.off()
   }    
   
   for (i in 1:dim(combn(1:max_dim,2))[2]) {
     
-    gp<-plot.PCA(resExp, axes = combn(1:max_dim,2)[,i], habillage = "ind", col.hab = colors,
-                 ggoptions = list(size=1.5),...)
+    gp<-plot.PCA(resExp, axes = combn(1:max_dim,2)[,i], habillage = "ind", col.hab = colors, title = main,
+                 ggoptions = list(size=1.75))
     ggsave(paste0(save_path,image_prefix,i,".png"), device = "png", plot = gp)
     
   }
-
+ return(resExp)
 }
 
 ##############
@@ -181,86 +182,33 @@ plotGenes <- function(expData, title = "", yMax = NULL, meanProfile = TRUE){
 ###################
 # Clustering
 ####################
-# Fonction 1 : Lecture des données
-F1_lecture_donnée <- function(Nom_de_fichier, Chemin_acces = "./"){
-  
-  if (is.character(Nom_de_fichier)){
-    expMatrix = read.table(paste0(Chemin_acces,Nom_de_fichier), header = T, row.names = 1)
-  }else{
-    expMatrix = Nom_de_fichier
-  }
 
-  # Si  des gènes ne sont pas exprimer
-  if (is.element(T, rowSums(expMatrix) <= ncol(expMatrix))){
-    # Mettre à part les genes qui ne sont pas exprimés
-    expMatrix_1 = expMatrix[rowSums(expMatrix) <= ncol(expMatrix),]
-    
-    # Supprimer les genes qui ne sont pas exprimés
-    expMatrix = expMatrix[rowSums(expMatrix) > ncol(expMatrix),]
-    
-    return(list(expMatrix, expMatrix_1)) 
-    # L'element retourné par la fonction est une liste contantant les 2 tableau de donnée (celui des gènes non eximer et celui des autres gènes)
-  } else {
-    return(expMatrix)
-    # L'élement retourné est le jeu de donné entier
-  }
-  
-}
-
-
+#install.packages("pheatmap")
+library("pheatmap")
 # Fonction F2 : Calcul de la matrice de distance
-F2_matrice_distance <- function(data, distance){
+F2_matrice_distance <- function(data_tab, distance){
   # Choisir le mode de calcule des distances
-  if (distance == "Euclidean"){
-    matDist = dist(data)
-  }else if (distance == "Correlation"){
-    matDist = as.dist(1 - cor(t(data)))
-  } 
-}
-
-
-# Fonction 3 : Application de l’algorithme de regroupement
-F3_Algorithme_regroupement <- function(matDist,expMatrix, nb_cluster, method){
-  # Choisir le type d'algorithme utilisé pour faire les clusters
-  if (method  == "kmeans"){
+  if (distance == "Pearson"){
+    matDist = as.matrix(cor(data_tab))
+    pheatmap(matDist, main = "Pheatmap Pearson")
+    matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
     
-    res = kmeans(matDist, nb_cluster)
-    vecCluster = res$cluster
-
-  }else if(method  == "HCL"){
-    res = hclust(matDist)
-    vecCluster = cutree(res, nb_cluster)
-    
-    #Fait un dendrogramme
-    plot(res)
+  }else if (distance == "Spearman"){
+    matDist = as.matrix(cor(data_tab,method="spearman"))
+    pheatmap(matDist, main = "Pheatmap Spearman")
+    matDist = as.dist(1-cor(log2(data_tab+1), method="spearman"))
   }
-  
-      
-    fviz_cluster(res,data = expMatrix,               
-                 geom = "point",
-                 ellipse.type = "convex", 
-                 ggtheme = theme_bw()
-                 )
 }
-
-
-# Fonction 4 : Extraction des profils de gènes pour un cluster donné
-F4_Extraction_profil_un_cluster <- function(data, vecCluster, numero_de_cluster){
-  geneCluster = names(which(vecCluster == numero_de_cluster))
-  cluster = data[geneCluster,]
-}
-
 
 
 # Fonction 5 : Représentation graphique des résultats
-F5_Representation_graphique <- function(data, cluster, graph_type, selected_cluster, distance, method, nb_cluster){
-  
+F5_Representation_graphique <- function(expMatrix, cluster, graph_type, selected_cluster, distance, method, nb_cluster){
   if (is.element(TRUE,graph_type == "profils")) {
     plotGenes(cluster, 
               title = paste("Cluster", selected_cluster, "\n",
                             "Distance :",distance,"-",
                             "Algorithme :", method, "avec ", nb_cluster, "clusters"),
-              #yMax = max(data)
+              #yMax = max(expMatrix)
               yMax = max(cluster)
     ) 
   }
@@ -271,7 +219,7 @@ F5_Representation_graphique <- function(data, cluster, graph_type, selected_clus
     if (nrow(cluster)>1){ 
       
       heatmap(as.matrix(cluster),
-              Colv = NA, Rowv = NA,
+              Colv = NA, Rowv = NA, cexCol = 0.2,
               main  = paste("\n","\n","Cluster", selected_cluster,"\n","Distance :",distance,"-",
                             "Algorithme :", method,  nb_cluster, "clusters"))
     }
@@ -281,48 +229,43 @@ F5_Representation_graphique <- function(data, cluster, graph_type, selected_clus
 
 
 # Fonction finale : fonction permettant de lancer les fonctions précédentes dans l'ordre et qui vas créer les graph pour tous les clusters
-Clustering <- function(Nom_de_fichier, Chemin_acces = "./",
+Clustering <- function(data_tab, Chemin_acces = "./",
                             distance, nb_cluster, method, graph_type){
   
-  # Permet de gérer le type de sortie de l'ouverture des fichiers selon qu'1 ou 2 tableau soient générer par la fonction F1
-  if(is.list(F1_lecture_donnée(Nom_de_fichier))){
-    expMatrix = F1_lecture_donnée(Nom_de_fichier)[[1]]
-    expMatrix_1 = F1_lecture_donnée(Nom_de_fichier)[[2]]
-  } else {
-    expMatrix = F1_lecture_donnée(Nom_de_fichier)
-    expMatrix_1 = NULL
+
+  ## Créaction de la matrice de distance
+  matDist = F2_matrice_distance(data_tab, distance)
+  
+  ## Créaction d'un vecteur contennat le clusering calculé a partir de la matrice de distance
+  # Choisir le type d'algorithme utilisé pour faire les clusters
+  if (method  == "kmeans"){
+    res = kmeans(matDist, nb_cluster)
+    vecCluster = res$cluster
+    
+  }else if(method  == "HCL"){
+    res = hclust(matDist)
+    vecCluster = cutree(res, nb_cluster)
+    
+    #Fait un dendrogramme
+    p= plot(res, main = paste("Dendrogramme", nb_cluster, "cluster calculé par", method, distance))
+    print(p)
   }
   
-  matDist = F2_matrice_distance(expMatrix, distance)
-  
-  vecCluster = F3_Algorithme_regroupement(matDist,expMatrix,nb_cluster, method)
-  
+  ## Sépare la fentêtre de grahique en 4 seulemnt si seul les profils sont demmandés
   if (is.element(FALSE,graph_type == "profils")){
     par(mfrow = c(1,1))
   }else {
     par(mfrow = c(2,2))
   }
   
-  # Si un tableau avec les données des gène non exrpimé est générer alors on fait les graphiques correspondnats
-  if(!is.null(expMatrix_1)){
-    vecCluster_0 = rep(0, nrow(expMatrix_1))
-    names(vecCluster_0) = rownames(expMatrix_1)
-    
-    cluster = F4_Extraction_profil_un_cluster(expMatrix_1, vecCluster_0,
-                                              0)
-    F5_Representation_graphique(expMatrix_1, cluster, graph_type,
-                                0,
-                                distance, method, nb_cluster)
-    
-  }
-  
-  # Faire les graphiques pour chacun des clusters générés la la fonction F3
+
+  ## Faire les graphiques pour chacun des clusters générés la la fonction F3
   for (selected_cluster in 1:nb_cluster){
-    cluster = F4_Extraction_profil_un_cluster(expMatrix, vecCluster,
-                                              selected_cluster)
+    geneCluster = names(which(vecCluster == selected_cluster))
+    cluster = data_tab[,geneCluster]
     
-    F5_Representation_graphique(expMatrix, cluster, graph_type,
-                                selected_cluster,
+    F5_Representation_graphique(data_tab, cluster, 
+                                graph_type, selected_cluster, 
                                 distance, method, nb_cluster)
   }
 

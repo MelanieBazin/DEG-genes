5b722557ea9e6e41c10828758e2b867d36fc2f6a
MelanieBazin <melanie19.bazin@gmail.com>
2021-05-12 18:13:05 +0200

    Mise en focrme des onnée clsuteriser pour l'analyse DEseq2

--- 3_Mise_en_forme_des_donnees.R
+++ 3_Mise_en_forme_des_donnees.R
@@ -1,36 +1,18 @@
-source("2_Fonction.R")
+source("0_Visualisation_des_donnees_fonction.R", encoding = "UTF-8")
 
+path = "./Analyse/DESeq2_test01"
 annotation = read.table("./DATA/My_annotation.tab",header=T,sep="\t")
 
-#### Definition des paramètres selon l'ARNi étudier ########
-if (condition == "CTIP") {
-  # Sélectionner les points à regrouper comme faisant parti du même timing
-  timing_ctrl = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",2))
-  timing_rnai = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",1))
+# Definition des paramètres selon l'ARNi étudier ########
+source("0_Cluster.R")
 
-}else if (condition == "XRCC4"){
-  # Sélectionner les points à regrouper comme faisant parti du même timing
-  timing_ctrl = c(rep("VEG",1),rep("EARLY",1),rep("INTER",1),rep("LATE",2))
-  timing_rnai = timing_ctrl
+# Choix du groupes de donnée à analysé
+i= names(rnai_list)[1]
 
-}else if (condition == "PGM"){
-  # Sélectionner les points à regrouper comme faisant parti du même timing
-  timing_ctrl_nd7 = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
-  timing_ctrl_icl7 = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
-  timing_ctrl = c(timing_ctrl_nd7, timing_ctrl_icl7)
-  timing_rnai = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
-  
-}else if (condition == "KU80c"){
-  # Sélectionner les points à regrouper comme faisant parti du même timing
-  timing_ctrl_nd7 = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
-  timing_ctrl_icl7 = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
-  timing_ctrl = c(timing_ctrl_nd7, timing_ctrl_icl7)
-  timing_rnai = c(rep("VEG",1),rep("EARLY",1),rep("INTER",2),rep("LATE",3))
-
-}
-
-#Ouverture des fichier et création de l'objet countdata
-countdata = OpenDataCount(path, condition)
+# Ouverture des fichiers et création de l'objet countdata
+countdata = ConcatTab("EXPRESSION", conditions = rnai_list[[i]])
+row.names(countdata)=countdata$ID
+countdata=countdata[,-1]
 
 ####### Sélection de gènes #####
 # selection = c("Ku","ku","PGM","NOWA","PTIWI","mt","TFIIS4","Spo11","Mre11","CER","Rad51", "Lig", "EZL", "SPT", "DCL", "CtIP", "XRCC4", "PDSG2", "PolX", "CAF1")
@@ -42,20 +24,36 @@ countdata = OpenDataCount(path, condition)
 #   
 # }
 # countdata = countdata[is.element(rownames(countdata), selection_ID),]
+#############################
 
+# Boxplot des comptages non-normalisés
+png(paste0(path,"Graph/",i,"_Row_Boxplot.png"))
+  CountBoxplot(countdata, "DESeq2")
+dev.off()
+
+# Création du tableau avec les info des colonnes
+infodata=CreatInfoData2(conditions = rnai_list[[i]])
 
 ####### Mise en forme des données pour DESeq2 ##############
 countdata =  as.matrix(countdata)
+library(DESeq2)
+deseq = DESeqDataSetFromMatrix(countData = countdata,
+                               colData  = infodata,
+                               design   = ~ RNAi + Timing)
 
-# Création du tableau avec les info des colonnes
-infodata = matrix(NA,nrow = ncol(countdata), ncol = 4)
-row.names(infodata) = colnames(countdata)
-colnames(infodata) = c("Noms", "Feeding", "Timing", "Conditions")
-infodata[,"Noms"] = colnames(countdata)
 
-infodata[,"Feeding"] = c(rep("controle",length(grep("CTRL", colnames(countdata)))),
-                         rep(paste("RNAi",condition),length(grep("RNAi", colnames(countdata)))))
-infodata[,"Timing"] = c(timing_ctrl,timing_rnai)
-infodata[,"Conditions"] = c(paste("CTRL", timing_ctrl, sep = "_" ),paste(condition, timing_rnai, sep = "_" ))
+# Analyse DESeq2
+deseq = DESeq(deseq)
 
-infodata = as.data.frame(infodata)
+# Graphique du paramètre de dispersion
+png(paste0(path,"Graph/",i,"_dipression_DESeq2.png"))
+  plotDispEsts(deseq, ylim = c(1e-6, 1e1))
+dev.off()
+
+# Récupération des données de comptage normalisées
+tab=counts(deseq,normalized=T)
+
+# Boxplot des comptages normalisés
+png(paste0(path,"Graph/",i,"_DESeq2_Boxplot.png"))
+  CountBoxplot(tab, "DESeq2")
+dev.off()

49ca7b168e7b75f5a48d06df53d433e82708f7b6
MelanieBazin <melanie19.bazin@gmail.com>
2021-06-05 04:37:36 +0200

    Commenter

--- 1_Expression_table.R
+++ 1_Expression_table.R
@@ -3,17 +3,18 @@ library(gplots)
 source("2_Visualisation_des_donnees_fonction.R", encoding = "UTF-8")
 annotation = read.table("./DATA/My_annotation2.tab",header=T,sep="\t")
 
-data_path = "DATA/RNAseq/"
+data_path = "./DATA/RNAseq/"
 save_path = "./Analyse/RowProfils/"
 
-dir.create("./DATA/EXPRESSION",recursive=T,showWarnings=F)
+dir.create("./DATA/EXPRESSION/",recursive=T,showWarnings=F)
 dir.create(save_path,recursive=T,showWarnings=F)
 
 data_directories = list.files(data_path)
-i = data_directories[1]
+
 for (i in data_directories){
   list= list.files(paste0(data_path,i))
   
+  # Extraire les timing des nom des fichier
   timing = str_split(list, '_', simplify=TRUE)
   for(a in c(1,4)){
    if(length(unique(timing[,a]))>1){
@@ -23,7 +24,8 @@ for (i in data_directories){
   timing = timing[,time]
   x = str_split(timing, '-', simplify=TRUE)[1,1]
   timing = gsub(paste0(x,"-"), "",timing)
-
+  
+  # Ouvrir les fichier et faire un tableau par condition
   table = data.frame(annotation$ID)
   colnames(table)="ID"
   for(j in list){
@@ -36,12 +38,18 @@ for (i in data_directories){
     table = table[,-1]
   }
   colnames(table) = timing
+  # Réordonner les colonnes par ordre chonologique
   colorder = c(ncol(table),order(as.numeric(gsub("T", "", colnames(table)[-ncol(table)]))))
   table = table[,colorder]
+  colnames(table)[1] = "Veg"
   
+  # Retirer les lignes qui ne correspondent pas à un gènes
   table = table[which(is.element(rownames(table),annotation$ID)),]
-  write.table(table,paste0("./DATA/EXPRESSION",i,".tab"),sep="\t", row.names=T,quote=F)
   
+  # Créer un nouveau tableau
+  write.table(table,paste0("./DATA/EXPRESSION/",i,".tab"),sep="\t", row.names=T,quote=F)
+} 
+  # Faire plot, box, plot et heatmap sur les row data pour verifier s'il ya un problème
   if(!is.element(F, rownames(table)==annotation$ID)){
     data_log =  as.matrix(log(table+1))
     expr_profil = unique(annotation$EXPRESSION_PROFIL)
@@ -64,8 +72,7 @@ for (i in data_directories){
         print(graph)
       dev.off()
     }
-    
-    
+
   }
 }
 

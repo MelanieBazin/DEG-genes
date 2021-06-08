0d0668d3b08b69a9c5ec0d249ceb45f48e9afef7
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2021-06-08 17:26:17 +0200

    Réorganisation et commentaires

--- 2_Visualisation_des_donnees_fonction.R
+++ 2_Visualisation_des_donnees_fonction.R
@@ -1,5 +1,8 @@
 options(stringsAsFactors = FALSE)
-source("0_Cluster.R")
+source("./0_Cluster.R")
+library("stringr") 
+
+###### Création/Modification tableau de comptage ####
 
 ConcatTab <- function(type, conditions = NULL){
   annotation = read.table("./DATA/My_annotation2.tab",header=T,sep="\t")
@@ -45,33 +48,44 @@ DivideByGeneSeize <- function(countdata){
   return(data_tab_seize)
 }
 
-CountBoxplot <- function (tab, type, color = "lightgray"){
-  boxplot(log(tab + 1), ylab = "count values (log scale)",
-          main = paste0("Count data (",type,")"), xaxt="n", yaxt="n",
-          col = color, outline=FALSE)
-  axis(side = 1, labels = FALSE, tick = F)
-  axis(side = 2,
-       ## Rotate labels perpendicular to y-axis.
-       las = 2,
-       ## Adjust y-axis label positions.
-       mgp = c(3, 0.75, 0))
-  text(x = 1:ncol(tab),
-       ## Move labels to just below bottom of chart.
-       y = par("usr")[3] - 0.1,
-       ## Use names from the data list.
-       labels = colnames(tab),
-       ## Change the clipping region.
-       xpd = NA,
-       ## Rotate the labels by 35 degrees.
-       srt = 90,
-       ## Adjust the labels to almost 100% right-justified.
-       adj = 1,
-       ## Increase label size.
-       cex = 0.5)
+MeanTabCalculation <- function(data_tab, rnai_list, cluster,i){
+  
+  mean_data_tab =data.frame(ID=rownames(data_tab))
+  c_split = c()
+  
+  for (a in rnai_list[[i]]){
+    tab = data_tab[,grep(a, colnames(data_tab))] # Récupération des colonnes correspodant a une cinétique
+    colnames(tab) = cluster[[a]] # Donner le nom des groupes de points aux colonnes
+    
+    
+    # Calculer la moyenne pour les points que l'on veux grouper dans la cinétique
+    mean_tab =data.frame(ID=rownames(data_tab))
+    for (b in unique(cluster[[a]])){
+      temp = grep(b, colnames(tab))
+      if(length(temp) == 1){
+        mean_tab[,b] = tab[,temp]
+      }else{
+        mean_tab[,b] = apply(tab[,temp], 1, mean)
+      }
+    }
+    
+    mean_tab = mean_tab[,c(1,ncol(mean_tab),2:(ncol(mean_tab)-1))]
+    colnames(mean_tab)[2:ncol(mean_tab)]=paste0(a,"_",colnames(mean_tab)[2:ncol(mean_tab)])
+    mean_data_tab = merge.data.frame(mean_data_tab, mean_tab, by = "ID")
+    rm(mean_tab, tab)
+  }
+  
+  # Passage de la colonne des ID en rowname
+  if (colnames(mean_data_tab)[1]=="ID"){
+    rownames(mean_data_tab)=mean_data_tab$ID
+    mean_data_tab = mean_data_tab[,-1]
+  }
+  mean_data_tab = as.matrix(mean_data_tab)
+  return(mean_data_tab)
+  
 }
 
-
-library(stringr)
+##### Création tabeau info avec les métadonnées #####
 CreatInfoData1 <- function(countdata, conditions, rnai_list, cluster){
   infodata = matrix(NA,nrow = ncol(countdata), ncol = 5)
   row.names(infodata) = colnames(countdata)
@@ -190,19 +204,56 @@ CreatInfoData3 <- function(countdata, conditions, rnai_list, cluster){
 return(infodata)
 }
 
+##### Analyse multi-variée #####
+# ACP_plot_generator fait par Olivier
+library(FactoMineR)
+library("factoextra")
+library(ggplot2)
+library(gtools)
+
+PCA_plot_generator <- function(Expression_Mat, colors,save_path, main,max_dim=3,barplot_max_dim=3,image_prefix="PCA_",show_barplot=T, vline=0, ...) {
+  save_path = paste0(save_path,"/ACP/")
+  dir.create(save_path,recursive=T,showWarnings=F)
+  
+  resExp = PCA(t(Expression_Mat), graph = F)
+  
+  if(show_barplot) {
+    eigenvalues <- resExp$eig
+    png(paste0(save_path,image_prefix,"_PCA_Variance.png"))
+      barplot(eigenvalues[1:barplot_max_dim, 2], names.arg=1:barplot_max_dim, 
+              main = "Variances",
+              xlab = "Principal Components",
+              ylab = "Percentage of variances",
+              col ="gray")
+      if(vline!=0) {
+        abline(v=vline,lty=2,lwd=2)
+      }
+    dev.off()
+  }    
+  
+  for (i in 1:dim(combn(1:max_dim,2))[2]) {
+    
+    gp<-plot.PCA(resExp, axes = combn(1:max_dim,2)[,i], habillage = "ind", col.hab = colors, title = main,
+                 ggoptions = list(size=2))
+    ggsave(paste0(save_path,image_prefix,i,".png"), device = "png", plot = gp)
+    
+  }
+ return(resExp)
+}
+
 library(ggplot2)
 library(ggrepel)
 
 LDA_plot_generator <- function(type = "LDA",lda_data_tab,infodata, lda_model, path, condition, color){
   path = paste0(path,"/",type,"/")
   dir.create(path,recursive=T,showWarnings=F)
-
+  
   png(paste0(path,condition,"_",type,"_hist.png"),width = 480, height = 1000)
-    plot(lda_model, dimen = 1, type = "b")
+  plot(lda_model, dimen = 1, type = "b")
   dev.off()
   # plot(lda_model, col = color, dimen = 2)
   prediction = predict(lda_model)$x
-
+  
   
   gg_data_tab = cbind(as.data.frame(lda_data_tab), prediction)
   gp = ggplot(gg_data_tab, aes(LD1, LD2))+
@@ -228,7 +279,7 @@ LDA_plot_generator <- function(type = "LDA",lda_data_tab,infodata, lda_model, pa
     theme_light()+
     scale_color_manual(values = unique(color))
   ggsave(paste0(path,condition,"_",type,"3.png"), device = "png", plot = gp, width = 20, height = 20, units = "cm")
- 
+  
   # gp = ggplot(gg_data_tab, aes(LD1, LD4))+
   #   geom_point(size = 1, aes(color = infodata$Cluster)) +
   #   geom_text_repel(size = 2, max.overlaps = 30 , aes(label = row.names(lda_data_tab), colour = infodata$Cluster))+
@@ -257,7 +308,7 @@ LDA_plot_generator <- function(type = "LDA",lda_data_tab,infodata, lda_model, pa
 
 
 EvaluPrediction <- function(type, data_tab, infodata , i, path){
-    if (type == "LDA"){
+  if (type == "LDA"){
     lda_data_tab=scale(t(data_tab))
     
     keep = c()
@@ -298,7 +349,7 @@ EvaluPrediction <- function(type, data_tab, infodata , i, path){
   dir.create(path,recursive=T,showWarnings=F)
   
   LDA_plot_generator(type,train,infodata[training_sample,], lda_model, paste0(path,"Train"), i, color)
-
+  
   info_train = infodata$Cluster[training_sample]
   info_test = infodata$Cluster[!training_sample]
   
@@ -324,7 +375,7 @@ EvaluPrediction <- function(type, data_tab, infodata , i, path){
   lda_test  = factor(lda_test, levels = unique(infodata$Cluster))
   info_train = factor(info_train, levels = unique(infodata$Cluster))
   info_test = factor(info_test, levels = unique(infodata$Cluster))
-
+  
   conf_mat_train = confusionMatrix(data = lda_train, reference = info_train)
   conf_mat_test = confusionMatrix(data = lda_test, reference = info_test)
   
@@ -335,180 +386,13 @@ EvaluPrediction <- function(type, data_tab, infodata , i, path){
   
   
   
-
-}
-
-MeanTabCalculation <- function(data_tab, rnai_list, cluster,i){
-
-  mean_data_tab =data.frame(ID=rownames(data_tab))
-  c_split = c()
-  
-  for (a in rnai_list[[i]]){
-    tab = data_tab[,grep(a, colnames(data_tab))] # Récupération des colonnes correspodant a une cinétique
-    colnames(tab) = cluster[[a]] # Donner le nom des groupes de points aux colonnes
-    
-    
-    # Calculer la moyenne pour les points que l'on veux grouper dans la cinétique
-    mean_tab =data.frame(ID=rownames(data_tab))
-    for (b in unique(cluster[[a]])){
-      temp = grep(b, colnames(tab))
-      if(length(temp) == 1){
-        mean_tab[,b] = tab[,temp]
-      }else{
-        mean_tab[,b] = apply(tab[,temp], 1, mean)
-      }
-    }
-    
-    mean_tab = mean_tab[,c(1,ncol(mean_tab),2:(ncol(mean_tab)-1))]
-    colnames(mean_tab)[2:ncol(mean_tab)]=paste0(a,"_",colnames(mean_tab)[2:ncol(mean_tab)])
-    mean_data_tab = merge.data.frame(mean_data_tab, mean_tab, by = "ID")
-    rm(mean_tab, tab)
-  }
-  
-  # Passage de la colonne des ID en rowname
-  if (colnames(mean_data_tab)[1]=="ID"){
-    rownames(mean_data_tab)=mean_data_tab$ID
-    mean_data_tab = mean_data_tab[,-1]
-  }
-  mean_data_tab = as.matrix(mean_data_tab)
-  return(mean_data_tab)
   
 }
 
-library("stringr")  
-library("pheatmap")
-library("ComplexHeatmap")
-library("magick")
-library("RColorBrewer")
-library(circlize)
-library(gplots)
 
+##### Clustering #####
 
-
-
-MyHeatmaps <- function(path, data_tab, moyenne = F, condition, Log = T){
-  dir.create(path,recursive=T,showWarnings=F)
-  
-  annotation = read.table("./DATA/My_annotation2.tab",header=T,sep="\t")
-  annotation = annotation[which(is.element(annotation$ID, rownames(data_tab))),]
-  
-  
-  if (Log == T){
-    data_log =  as.matrix(log(data_tab+1))
-    Ylab = "log(expres)"
-  }else{
-    data_log =  as.matrix(data_tab)
-    Ylab = "expres"
-  }
-
-  color_vec = c(0,quantile(data_log)[[2]],median(data_log), 
-                quantile(data_log)[[4]],max(data_log))
-  
-  c_split = vector("character",ncol(data_log))
-  c_order = c()
-  c_order_ctr = c()
-  for (a in rnai_list[[condition]]){
-    x = grep(a, colnames(data_log))
-    c_split[x] = c(rep(a,length(x)))
-    
-    if (length(grep("ND7",a)) > 0 | length(grep("ICL7",a)) > 0){
-      c_order_ctr = c(c_order_ctr,x)
-    }else{
-      c_order = c(c_order, x)
-    }
-  }
-  c_order = c(c_order_ctr, c_order)
-  
-  h = Heatmap(data_log,
-              name = Ylab,
-              col = colorRamp2(color_vec, c("white","#FEE0D2","#FB6A4A","#BD0026","#67000D")),
-              cluster_rows = F, # turn off row clustering
-              cluster_columns = F, # turn off column clustering
-              column_title = condition,
-              show_row_names = F,
-              row_order = order(annotation$EXPRESSION_PROFIL),
-              row_split = annotation$EXPRESSION_PROFIL,
-              row_title = "%s", row_title_rot = 0,
-              column_split = c_split,
-              column_order = c_order,
-              use_raster = T)
-  h2 = Heatmap(data_log,
-               name = Ylab,
-               cluster_rows = F, # turn off row clustering
-               cluster_columns = F, # turn off column clustering
-               column_title = condition,
-               show_row_names = F,
-               row_order = order(annotation$EXPRESSION_PROFIL),
-               row_split = annotation$EXPRESSION_PROFIL,
-               row_title = "%s", row_title_rot = 0,
-               column_split = c_split,
-               column_order = c_order,
-               use_raster = T)
-  
-  if (moyenne == T){
-    moyenne = "MOYENNE"
-  }else{
-    moyenne = ""
-  }
-  
-  png(paste0(path,condition,"_AllPoint_",moyenne,"heatmap_red.png"),width = 500, height = 600)
-    print(h)
-  dev.off()
-  
-  png(paste0(path,condition, "_AllPoint_",moyenne,"heatmap_blue.png"),width = 500, height = 600)
-    print(h2)
-  dev.off()
-}
-
-#############################################
-# PCA
-#############################################
-#Olivier
-
-
-library(FactoMineR)
-library("factoextra")
-library(ggplot2)
-library(gtools)
-
-PCA_plot_generator <- function(Expression_Mat, colors,save_path, main,max_dim=3,barplot_max_dim=3,image_prefix="PCA_",show_barplot=T, vline=0, ...) {
-  save_path = paste0(save_path,"/ACP/")
-  dir.create(save_path,recursive=T,showWarnings=F)
-  
-  resExp = PCA(t(Expression_Mat), graph = F)
-  
-  if(show_barplot) {
-    eigenvalues <- resExp$eig
-    png(paste0(save_path,image_prefix,"_PCA_Variance.png"))
-      barplot(eigenvalues[1:barplot_max_dim, 2], names.arg=1:barplot_max_dim, 
-              main = "Variances",
-              xlab = "Principal Components",
-              ylab = "Percentage of variances",
-              col ="gray")
-      if(vline!=0) {
-        abline(v=vline,lty=2,lwd=2)
-      }
-    dev.off()
-  }    
-  
-  for (i in 1:dim(combn(1:max_dim,2))[2]) {
-    
-    gp<-plot.PCA(resExp, axes = combn(1:max_dim,2)[,i], habillage = "ind", col.hab = colors, title = main,
-                 ggoptions = list(size=2))
-    ggsave(paste0(save_path,image_prefix,i,".png"), device = "png", plot = gp)
-    
-  }
- return(resExp)
-}
-
-
-###################
-# Clustering
-####################
-
-
-Clustering <- function(matDist, nb_cluster, method, 
-                       titre, colors = NULL){
+Clustering <- function(matDist, nb_cluster, method, titre, colors = NULL){
   ## Créaction d'un vecteur contenant le clusering calculé a partir de la matrice de distance
   # Choisir le type d'algorithme utilisé pour faire les clusters
   if (method  == "kmeans"){
@@ -531,10 +415,146 @@ Clustering <- function(matDist, nb_cluster, method,
   
 }
 
-########## Fonction Gaëlle ####
+###### Création graphique ####
 
+# Crée des boxplot sur toues les gènes par catégories
+CountBoxplot <- function (tab, type, color = "lightgray"){
+  boxplot(log(tab + 1), ylab = "count values (log scale)",
+          main = paste0("Count data (",type,")"), xaxt="n", yaxt="n",
+          col = color, outline=FALSE)
+  axis(side = 1, labels = FALSE, tick = F)
+  axis(side = 2,
+       ## Rotate labels perpendicular to y-axis.
+       las = 2,
+       ## Adjust y-axis label positions.
+       mgp = c(3, 0.75, 0))
+  text(x = 1:ncol(tab),
+       ## Move labels to just below bottom of chart.
+       y = par("usr")[3] - 0.1,
+       ## Use names from the data list.
+       labels = colnames(tab),
+       ## Change the clipping region.
+       xpd = NA,
+       ## Rotate the labels by 35 degrees.
+       srt = 90,
+       ## Adjust the labels to almost 100% right-justified.
+       adj = 1,
+       ## Increase label size.
+       cex = 0.5)
+}
 
-# This function is useful to draw gene expression profiles
+# Fait les heatmap avec tous les gènes par actégories
+library("pheatmap")
+library("ComplexHeatmap")
+library("magick")
+library("RColorBrewer")
+library(circlize)
+library(gplots)
+MyHeatmaps <- function(path, data_tab, moyenne = F, condition, Log = T){
+  dir.create(path,recursive=T,showWarnings=F)
+  
+  annotation = read.table("./DATA/My_annotation2.tab",header=T,sep="\t")
+  annotation = annotation[which(is.element(annotation$ID, rownames(data_tab))),]
+  
+  
+  if (Log == T){
+    data_log =  as.matrix(log(data_tab+1))
+    Ylab = "log(expres)"
+  }else{
+    data_log =  as.matrix(data_tab)
+    Ylab = "expres"
+  }
+  
+  color_vec = c(0,quantile(data_log)[[2]],median(data_log), 
+                quantile(data_log)[[4]],max(data_log))
+  color_vec = colorRamp2(color_vec, 
+                         c("white","#FEE0D2","#FB6A4A","#BD0026","#67000D"))
+  
+  c_split = c()
+  c_split_ctr = c()
+  c_order = c()
+  c_order_ctr = c()
+  for (a in rnai_list[[condition]]){
+    x = grep(a, colnames(data_log))
+    
+    
+    if (length(grep("ND7",a)) > 0 | length(grep("ICL7",a)) > 0){
+      c_order_ctr = c(c_order_ctr,x)
+      c_split_ctr = c(c_split_ctr,rep(a,length(x)))
+    }else{
+      c_order = c(c_order, x)
+      c_split = c(c_split,rep(a,length(x)))
+    }
+  }
+  c_split = c(c_split_ctr, c_split)
+  c_order = c(c_order_ctr, c_order)
+  
+  data_log = cbind(apply(data_log, 1, max),data_log)
+  data_log = data_log[order(data_log[,1], decreasing = T),]
+  data_log = data_log[,is.element(colnames(data_log), colnames(data_tab))]
+  data_log = data_log[,c_order]
+  
+  h = Heatmap(data_log,
+              name = Ylab,
+              col = color_vec,
+              cluster_rows = F, # turn off row clustering
+              cluster_columns = F, # turn off column clustering
+              column_title = condition,
+              show_row_names = F,
+              row_order = NULL,
+              row_split = order(annotation$EXPRESSION_PROFIL),
+              row_title = "%s", row_title_rot = 0,
+              column_split = c_split,
+              column_order = NULL,
+              use_raster = T)
+  
+  h1 = Heatmap(data_log,
+              name = Ylab,
+              col = colorRamp2(color_vec, c("white","#FEE0D2","#FB6A4A","#BD0026","#67000D")),
+              cluster_rows = F,
+              cluster_columns = F, # turn off column clustering
+              column_title = condition,
+              show_row_names = F,
+              row_order = NULL,
+              row_split = order(annotation$EXPRESSION_PROFIL),
+              row_title = "%s", row_title_rot = 0,
+              column_split = c_split,
+              column_order = NULL,
+              use_raster = F)
+  
+  h2 = Heatmap(data_log,
+               name = Ylab,
+               cluster_rows = F, # turn off row clustering
+               cluster_columns = F, # turn off column clustering
+               column_title = condition,
+               show_row_names = F,
+               row_order = order(annotation$EXPRESSION_PROFIL),
+               row_split = annotation$EXPRESSION_PROFIL,
+               row_title = "%s", row_title_rot = 0,
+               column_split = c_split,
+               column_order = NULL,
+               use_raster = T)
+  
+  if (moyenne == T){
+    moyenne = "MOYENNE"
+  }else{
+    moyenne = ""
+  }
+  
+  png(paste0(path,condition,"_AllPoint_",moyenne,"heatmap_red.png"),width = 500, height = 600)
+    print(h)
+  dev.off()
+  
+  png(paste0(path,condition,"_AllPoint_",moyenne,"heatmap_red_unraster.png"),width = 500, height = 600)
+    print(h1)
+  dev.off()
+  
+  png(paste0(path,condition, "_AllPoint_",moyenne,"heatmap_blue.png"),width = 500, height = 600)
+    print(h2)
+  dev.off()
+}
+
+# plotGenes fait par Gaëlle
 plotGenes <- function(expData, title = "", yMax = NULL, meanProfile = TRUE){
   
   # Check function parameters
@@ -571,8 +591,7 @@ plotGenes <- function(expData, title = "", yMax = NULL, meanProfile = TRUE){
   # end of function plotGenes()  
 }
 
-
-
+# Dessine les profils des groupes de gènes + trace le profils moyen
 ProfilsPDF <- function(save_path = paste0(path,"/profils/"), data_tab, moyenne = F, condition, Log = T){
 
   annotation = read.table("./DATA/My_annotation2.tab",header=T,sep="\t")
@@ -684,6 +703,7 @@ ProfilsPNG <- function(save_path = paste0(path,"/profils/"), data_tab, moyenne =
   }
 }
 
+# Dessine les profils d'expression de tous les gènes pour tous les RNai demmandés
 ExpressionProfils <- function(type = "RPKM", rnai = NULL){
   dir.create("./Analyse/Profils/",recursive=T,showWarnings=F)
   

f40ee4527e1c40828cda163a7933312a982aef3a
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2021-06-17 21:10:27 +0200

    i -> condition

--- 3_Visualisation_des_donnees_new.R
+++ 3_Visualisation_des_donnees_new.R
@@ -4,26 +4,26 @@
 #  - des donnée avant et après clustering (heatmp et plot)
 #  - ACP, LDA, heatmap, clustering herarchique
 
-# i correspond aux names(rnai_list) et donc aux groupe de RNA-seq analysés ensembles
+#  condition correspond aux names(rnai_list) et donc aux groupe de RNA-seq analysés ensembles
 
 
 ##### Boxplot des comptages normalisés divisé par la taille des gènes #####
-print(paste(i, "-----> Creation BoxPlot normalise"))
-# pdf(paste0(path,i,"_DESeq_Boxplot.pdf"))
-png(paste0(path,i,"_DESeq_Boxplot.png"))
+print(paste( condition, "-----> Creation BoxPlot normalise"))
+# pdf(paste0(path, condition,"_DESeq_Boxplot.pdf"))
+png(paste0(path, condition,"_DESeq_Boxplot.png"))
 CountBoxplot(data_tab, "DESeq2_seize", color = c(rep("darkolivegreen2",28), rep("chartreuse4",21))) 
 dev.off()
 
-if (is.element(paste0(i,"_DESeq2-seize.tab"),list.files(paste0("./DATA/DESeq2-seize/")))){
-  data_tab_seize = read.table(paste0("./DATA/DESeq2-seize/",i,"_DESeq2-seize.tab"), header = T, sep = "\t")
+if (is.element(paste0( condition,"_DESeq2-seize.tab"),list.files(paste0("./DATA/DESeq2-seize/")))){
+  data_tab_seize = read.table(paste0("./DATA/DESeq2-seize/", condition,"_DESeq2-seize.tab"), header = T, sep = "\t")
 }else {
   print("Calcule de la table DESeq/taille")
   data_tab_seize = DivideByGeneSeize(data_tab)
-  write.table(data_tab_seize,paste0("./DATA/DESeq2-seize/",i,"_DESeq2-seize.tab"), sep="\t",row.names=F,quote=F)
+  write.table(data_tab_seize,paste0("./DATA/DESeq2-seize/", condition,"_DESeq2-seize.tab"), sep="\t",row.names=F,quote=F)
 }
 
-# pdf(paste0(path,i,"_DESeq-seize_Boxplot.pdf"))
-png(paste0(path,i,"_DESeq-seize_Boxplot.png"))
+# pdf(paste0(path, condition,"_DESeq-seize_Boxplot.pdf"))
+png(paste0(path, condition,"_DESeq-seize_Boxplot.png"))
 CountBoxplot(data_tab_seize, "DESeq2_seize", color = c(rep("darkolivegreen2",28), rep("chartreuse4",21))) 
 dev.off()
 
@@ -31,43 +31,43 @@ print("Boxplot fini")
 
 
 ##### Heatmap et profils  ####
-print(paste(i, "-----> Conception des heatmap"))
+print(paste( condition, "-----> Conception des heatmap"))
 
 # Avant moyenne par cluster
-ProfilsPNG(save_path = paste0(path,"profils/"), data_tab, condition = i)
-ProfilsPDF(save_path = paste0(path,"profils/"), data_tab, condition = i)
+ProfilsPNG(save_path = paste0(path,"profils/"), data_tab, condition =  condition)
+ProfilsPDF(save_path = paste0(path,"profils/"), data_tab, condition =  condition)
 
 
-MyHeatmaps(path = paste0(path,"Heatmap/"),data_tab, condition = i, sortie = "png")
-MyHeatmaps(paste0(path,"HeatmapNoLog/"),data_tab, condition = i, Log = F, sortie = "png")
+MyHeatmaps(path = paste0(path,"Heatmap/"),data_tab, condition =  condition, sortie = "png")
+MyHeatmaps(paste0(path,"HeatmapNoLog/"),data_tab, condition =  condition, Log = F, sortie = "png")
 
 # Avec calcul des moyennes sur les clusters
-mean_data_tab = MeanTabCalculation(data_tab, rnai_list, cluster,i)
+mean_data_tab = MeanTabCalculation(data_tab, rnai_list, cluster, condition)
 
-ProfilsPNG(save_path = paste0(path,"profils/"), mean_data_tab, moyenne = T, condition = i)
-ProfilsPDF(save_path = paste0(path,"profils/"), mean_data_tab, moyenne = T, condition = i)
+ProfilsPNG(save_path = paste0(path,"profils/"), mean_data_tab, moyenne = T, condition =  condition)
+ProfilsPDF(save_path = paste0(path,"profils/"), mean_data_tab, moyenne = T, condition =  condition)
 
-MyHeatmaps(paste0(path,"Heatmap/"),mean_data_tab, moyenne = T, condition = i, sortie = "png")
-MyHeatmaps(paste0(path,"HeatmapNoLog/"),mean_data_tab, moyenne = T, condition = i, Log = F, sortie = "png")
+MyHeatmaps(paste0(path,"Heatmap/"),mean_data_tab, moyenne = T, condition =  condition, sortie = "png")
+MyHeatmaps(paste0(path,"HeatmapNoLog/"),mean_data_tab, moyenne = T, condition =  condition, Log = F, sortie = "png")
 
 
 
 ##### Analyse multi-variée des données pour clustering  #####
 # Créaction du vecteur de couleur par cluster
 color = colnames(data_tab)
-for (j in rnai_list[[i]]){
+for (j in rnai_list[[ condition]]){
   color[grep(j, color)]=cluster_color[[j]]
 }
 
 # Analyse en composante principale
-print(paste(i, "-----> Analyse ACP"))
+print(paste( condition, "-----> Analyse ACP"))
 PCA_plot_generator(data_tab,colors = color,
                    save_path = path,
-                   main = paste0("ACP ",i," (DESeq2)"),
+                   main = paste0("ACP ", condition," (DESeq2)"),
                    sortie = "png")
 
 # Analyse de discrimination linéaire (LDA)
-print(paste(i, "-----> Analyse LDA"))
+print(paste( condition, "-----> Analyse LDA"))
 
 lda_data_tab=scale(t(data_tab)) #s'assurer que la sd est de 1 et la moyenne à 0 (prédicat des lda)
 # summary(apply(lda_data_tab,2,mean)) #verification que la moyenne est à 0 ou très proche
@@ -84,28 +84,28 @@ lda_model = lda(lda_data_tab, grouping = infodata$Cluster)
 # summary(lda_model$scaling)
 lda_pred = predict(lda_model)
 
-LDA_plot_generator("LDA",lda_data_tab,infodata, lda_model, path, i, color, sortie = "png")
+LDA_plot_generator("LDA",lda_data_tab,infodata, lda_model, path,  condition, color, sortie = "png")
 
 # print("Teste des prédictions")
-# EvaluPrediction("LDA", data_tab, infodata, i, path)  # Evaluer la prédiction
+# EvaluPrediction("LDA", data_tab, infodata,  condition, path)  # Evaluer la prédiction
 
 
 ##### Clustering hierarchique  #####
-print(paste(i, "-----> Clustering en cours"))
+print(paste( condition, "-----> Clustering en cours"))
 dir.create(paste0(path,"4Cluster/"),recursive=T,showWarnings=F)
 for (distance in c("Pearson", "Spearman")){
-  png(paste0(path,"4Cluster/",i,"_Matrice_",distance,".png"))
-  # pdf(paste0(path,"4Cluster/",i,"_Matrice_",distance,".pdf"))
+  png(paste0(path,"4Cluster/", condition,"_Matrice_",distance,".png"))
+  # pdf(paste0(path,"4Cluster/", condition,"_Matrice_",distance,".pdf"))
   # Choisir le mode de calcule des distances
   if (distance == "Pearson"){
     matDist = as.matrix(cor(data_tab))
-    p= pheatmap(matDist, main = paste("Pheatmap Pearson DESeq2", i))
+    p= pheatmap(matDist, main = paste("Pheatmap Pearson DESeq2",  condition))
     print(p)
     matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
     
   }else if (distance == "Spearman"){
     matDist = as.matrix(cor(data_tab,method="spearman"))
-    p= pheatmap(matDist, main = paste("Pheatmap Spearman DESeq2", i))
+    p= pheatmap(matDist, main = paste("Pheatmap Spearman DESeq2",  condition))
     print(p)
     matDist = as.dist(1-cor(log2(data_tab+1), method="spearman"))
   }
@@ -114,12 +114,12 @@ for (distance in c("Pearson", "Spearman")){
   
   for (method in c("kmeans", "HCL")){
     print(paste(distance, method))
-    png(paste0(path,"4Cluster/",i,"_Cluster_",method,"_",distance,".png"))
-    # pdf(paste0(path,"4Cluster/",i,"_Cluster_",method,"_",distance,".pdf"))
+    png(paste0(path,"4Cluster/", condition,"_Cluster_",method,"_",distance,".png"))
+    # pdf(paste0(path,"4Cluster/", condition,"_Cluster_",method,"_",distance,".pdf"))
     Clustering(matDist = matDist,
                nb_cluster = 5,
                method = method,
-               titre = paste("DESeq2",i),
+               titre = paste("DESeq2", condition),
                colors = color)
     dev.off()
   }

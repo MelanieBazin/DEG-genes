e4ad292ea8aa6dfcc5cfb17e70c614b4cf2a5b05
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2021-08-24 16:39:23 +0200

    ajout la possibilité de ne regader que les motif à un endroit donner dans le prom

--- 6_Compraison_prom.R
+++ 6_Compraison_prom.R
@@ -4,7 +4,7 @@ library(stringr)
 source("0_Cluster.R")
 
 # f = files[1]
-# type = "fuzznuc" #fuzznuc ou fimo
+# type = "fimo" #fuzznuc ou fimo
 
 path = "./Analyse/2021-07-07_Analyse_DESeq2_tout_CombatON_FC-1.5_pval-0.05/tout/"
 save_path = paste0(path, "MOTIF/Motif_dans_prom/Motif_intermed/Parmis_tous_les_genes/")
@@ -14,14 +14,13 @@ annotation = read.table("./DATA/My_annotation2.tab",header=T,sep="\t")
 colnamesgff3=c("ID","SOURCE","TYPE","START","END","SCORE","STRAND","PHASE", "ATTRIBUTES")
 
 
-for (type in c("fimo","fuzznuc")){
+for (type in c("fuzznuc", "fimo")){
   
   if(type == "fimo"){
     dossier  = "FIMO/"
     
   }else if (type == "fuzznuc"){
     dossier  = "FUZZNUC/"
-
   }
   
   files = list.files(paste0(save_path,dossier), pattern = ".gff")
@@ -33,22 +32,21 @@ for (type in c("fimo","fuzznuc")){
     colnames(prom_with_motif)=colnamesgff3
     
     tab_annot = merge(prom_with_motif, annotation, by = "ID")
+    tab_annot$START = tab_annot$START-150
+    tab_annot$END = tab_annot$END-150
     
     if(type == "fimo"){
       prom_with_motif2 = read.table(paste0(save_path,dossier,sub(".gff","",f),".tsv"), header=T, sep="\t")
       tab_annot2 = merge(prom_with_motif2, annotation, by.x = "sequence_name", by.y ="ID")
-      write.table(tab_annot2, paste0(save_path, sub(".gff","",f),"_motif.tab"), sep = "\t") 
+      tab_annot2$start = tab_annot2$start-150
+      tab_annot2$stop = tab_annot2$stop-150
+      write.table(tab_annot2, paste0(save_path, sub(".gff","",f),"_motif.tab"), sep = "\t", row.names = F) 
       
       tab_annot = tab_annot[order(tab_annot2$p.value),]
-      tab_annot_select = tab_annot[1:100,]
       
     }else if (type == "fuzznuc"){
-      write.table(tab_annot, paste0(save_path, sub(".gff3","",f),"_motif.tab"), sep = "\t") 
-      tab_annot_select = tab_annot
+      write.table(tab_annot, paste0(save_path, sub(".gff3","",f),"_motif.tab"), sep = "\t", row.names = F) 
     }
-    
-    # Selectonner seulment des gènes particulier
-    # tab_annot_select = tab_annot[is.element(tab_annot$ID, select_ID),]
     
     debut = str_sub(sub(".gff","",sub(".gff3","",f)), -3)
     if(debut == "CDS"){
@@ -60,22 +58,42 @@ for (type in c("fimo","fuzznuc")){
     
     promoteur = read.fasta(paste0("./DATA/Promoteur/IN_MAC_", IES, "upstream_150nt_",debut,".fa"))
     
-    for (strand in c("+","-")){
-      tab_annot_select_plus = tab_annot_select[tab_annot_select$STRAND == strand,]
-      
-      prom_select = promoteur[which(is.element(names(promoteur),tab_annot_select_plus$ID))]
-      prom_select = prom_select[order(tab_annot_select_plus$START)]
-      name = tab_annot_select_plus$NAME[order(tab_annot_select_plus$START)]
-      
-      for (i in na.omit(names(prom_select))){
-        prom_select[[i]] = c(rep("-",max(tab_annot_select_plus$START)-tab_annot_select_plus$START[grep(i,tab_annot_select_plus$ID)[1]]),prom_select[[i]])
-      }
-      
-      write.fasta(sequences = prom_select, names = name, file.out = paste0(save_path, sub(".gff","",sub(".gff3","",f)),"_prom",strand,".fa") )
-    }
     
+    for (strand in c("+","-")){
+      tab_annot_select = tab_annot[tab_annot$STRAND == strand,]
+      for(j in c("local", "loin","proche", "all")){
+        if (j == "all"){
+          tab_annot_select_plus = tab_annot_select
+        }else if (j == "local"){
+          tab_annot_select_plus = tab_annot_select[tab_annot_select$START >= -70 & tab_annot_select$START<= -30,]
+        }else if(j == "loin"){
+          tab_annot_select_plus = tab_annot_select[tab_annot_select$START < -70,]
+        }else if(j == "proche"){
+          tab_annot_select_plus = tab_annot_select[tab_annot_select$START > -30,]
+        }
+        if (nrow(tab_annot_select_plus) > 5){
+          if (nrow(tab_annot_select_plus)>=100){
+            tab_annot_select_plus = tab_annot_select_plus[1:100,]
+          }
+          
+          tab_tab_annot_select_plus = tab_annot_select_plus[order(tab_annot_select_plus$SCORE),]
+
+          prom_select = promoteur[which(is.element(names(promoteur),tab_annot_select_plus$ID))]
+          prom_select = prom_select[order(tab_annot_select_plus$START)]
+          name = tab_annot_select_plus$NAME[order(tab_annot_select_plus$START)]
+          
+          for (i in na.omit(names(prom_select))){
+            repet = max(tab_annot_select_plus$START)-max(tab_annot_select_plus$START[grep(i,tab_annot_select_plus$ID)])
+            prom_select[[i]] = c(rep("-",repet ),prom_select[[i]])
+          }
+          
+          write.fasta(sequences = prom_select, names = name, file.out = paste0(save_path, sub(".gff","",sub(".gff3","",f)),"_prom",strand,"_",j,".fa") )
+        }
+      }
+    }
   }
 }
+print ("- FINI ! -")
 
 ###### Notes #####
 #get_sequence_from_gff.pl -gff ~/DATA/ptetraurelia_mac_51_annotation_v2.0.gff3 -genome /data/PARAMECIUM/REFERENCES/tetraurelia/ptetraurelia_mac_51.fa -upstream 150 -type CDS -out IN_MAC_upstream_150nt_CDS_start.tab > IN_MAC_upstream_150nt_CDS_start.fa

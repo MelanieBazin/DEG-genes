e6b18632f9f183287106ad93198163419cddcc49
Melanie BAZIN <melanie.bazin@i2bc.paris-sacla.fr>
2021-09-14 11:00:41 +0200

    ajout génération des données ELZ1  + expression profiles

--- 5_Valeur_volcanoplot.R
+++ 5_Valeur_volcanoplot.R
@@ -3,6 +3,7 @@ all = 41533
 PGM = read.table(paste0("Analyse/2021-07-07_Analyse_DESeq2_tout_CombatON_FC-1.5_pval-0.05/tout/DESeq/","PGM","/NoFilter/DEgenes_tout_LATE_NoFilter.tab"), sep = "\t", h=T )
 KU80c = read.table(paste0("Analyse/2021-07-07_Analyse_DESeq2_tout_CombatON_FC-1.5_pval-0.05/tout/DESeq/","KU80c","/NoFilter/DEgenes_tout_LATE_NoFilter.tab"), sep = "\t", h=T)
 XRCC4 = read.table(paste0("Analyse/2021-07-07_Analyse_DESeq2_tout_CombatON_FC-1.5_pval-0.05/tout/DESeq/","XRCC4","/NoFilter/DEgenes_tout_LATE_NoFilter.tab"), sep = "\t", h=T )
+EZL1 = read.table(paste0("Analyse/2021-07-07_Analyse_DESeq2_tout_CombatON_FC-1.5_pval-0.05/tout/DESeq/","EZL1","/NoFilter/DEgenes_tout_LATE_NoFilter.tab"), sep = "\t", h=T )
 
 CTIP_e = read.table(paste0("Analyse/2021-07-07_Analyse_DESeq2_tout_CombatON_FC-1.5_pval-0.05/tout/DESeq/","CTIP","/NoFilter/DEgenes_tout_EARLY_NoFilter.tab"), sep = "\t", h=T )
 CTIP_i = read.table(paste0("Analyse/2021-07-07_Analyse_DESeq2_tout_CombatON_FC-1.5_pval-0.05/tout/DESeq/","CTIP","/NoFilter/DEgenes_tout_INTER_NoFilter.tab"), sep = "\t", h=T )
@@ -20,6 +21,9 @@ tab = table(XRCC4$REGULATION)
 print("XRCC4")
 print(rbind(tab,tab/all*100))
 
+tab = table(EZL1$REGULATION)
+print("EZL1")
+print(rbind(tab,tab/all*100))
 
 tab = table(CTIP_e$REGULATION)
 print("CTIP_e")
@@ -28,3 +32,23 @@ print(rbind(tab,tab/all*100))
 tab = table(CTIP_i$REGULATION)
 print("CTIP_i")
 print(rbind(tab,tab/all*100))
+
+
+tab2 = PGM[PGM$REGULATION == "Up-regulated",]
+table(tab2$EXPRESSION_PROFIL)
+
+tab2 = KU80c[KU80c$REGULATION == "Up-regulated",]
+table(tab2$EXPRESSION_PROFIL)
+
+tab2 = XRCC4[XRCC4$REGULATION == "Up-regulated",]
+table(tab2$EXPRESSION_PROFIL)
+
+tab2 = EZL1[EZL1$REGULATION == "Up-regulated",]
+table(tab2$EXPRESSION_PROFIL)
+
+tab2 = CTIP_e[CTIP_e$REGULATION == "Down-regulated",]
+table(tab2$EXPRESSION_PROFIL)
+
+tab2 = CTIP_i[CTIP_i$REGULATION == "Down-regulated",]
+table(tab2$EXPRESSION_PROFIL)
+

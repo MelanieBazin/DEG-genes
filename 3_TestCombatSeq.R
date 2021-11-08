ec938522e2adb7e052c338af106535172defcb0a
Melanie BAZIN <melanie.bazin@i2bc.paris-sacla.fr>
2021-11-08 23:49:54 +0100

    Ajout possibilité pour faire tourner le code sur "tout" et pas uniquement sur les données test sequancer 2 fois

--- 3_TestCombatSeq.R
+++ 3_TestCombatSeq.R
@@ -19,7 +19,7 @@ analyseName = paste0(Sys.Date(),"_", analyseName)
 path_dir = paste0("./Analyse/",analyseName,"/")
 dir.create(path_dir,recursive=T,showWarnings=F)
 
-condition = names(rnai_list)[2]
+condition = names(rnai_list)[1]
 
 print(paste("On analyse le jeu de donnee :", condition , "-->", paste(rnai_list[[condition]], collapse = ", ") ))
 
@@ -33,10 +33,12 @@ countdata = read.table(paste0("./DATA/Pour_DESeq_SansCorrectionBatch/",condition
 # Création du tableau avec les info des colonnes
 infodata = CreatInfoData3(countdata, conditions = condition , rnai_list, cluster)
 
-# Se limiter au données présentes dans les 2 séquancages
-time = unique(infodata$Timing[which(infodata$Batch == "seq_2020")])
-countdata = countdata[,is.element(infodata$Timing, time)]
-infodata = infodata[is.element(infodata$Timing, time),]
+if (condition == names(rnai_list)[2]){
+  # Se limiter au données présentes dans les 2 séquancages
+  time = unique(infodata$Timing[which(infodata$Batch == "seq_2020")])
+  countdata = countdata[,is.element(infodata$Timing, time)]
+  infodata = infodata[is.element(infodata$Timing, time),]
+}
 
 # Creataion de l'objet DESeq2
 countdata = as.matrix(countdata)
@@ -58,11 +60,19 @@ seq_2014 = "chartreuse4"
 seq_2020 = "blue4"
 
 seq_color = c()
-for (j in rnai_list[[condition]]){
-  if (is.element(j,c("ICL7bis", "EZL1bis"))){
-    seq_color=c(seq_color,rep(seq_2020,length(time)))
-  }else if (is.element(j, c("ICL7", "EZL1"))){
-    seq_color=c(seq_color,rep(seq_2014, length(time)))
+
+if (condition == names(rnai_list)[2]){
+  for (j in rnai_list[[condition]]){
+    if (is.element(j,c("ICL7bis", "EZL1bis"))){
+      seq_color=c(seq_color,rep(seq_2020,length(time)))
+    }else if (is.element(j, c("ICL7", "EZL1"))){
+      seq_color=c(seq_color,rep(seq_2014, length(time)))
+    }
+  }
+}else {
+  for (j in rnai_list[[condition]]){
+    
+    seq_color=c(seq_color,cluster_color[[j]])
   }
 }
 names(seq_color) = colnames(data_tab)
@@ -80,14 +90,14 @@ early_color = "deepskyblue"
 inter_color = "chartreuse3"
 late_color = "red"
 very_late_color = "deeppink2"
-
-cluster = list(
-  ICL7 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
-  ICL7bis = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
-  EZL1 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
-  EZL1bis = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2))
-)
-
+if (condition == names(rnai_list)[2]){
+  cluster = list(
+    ICL7 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
+    ICL7bis = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
+    EZL1 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
+    EZL1bis = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2))
+  )
+}
 
 clust_color = c()
 for (j in rnai_list[[condition]]){
@@ -150,197 +160,201 @@ for (distance in c("Pearson", "Spearman")){
 
 
 ##### AVEC correction de l'effet Batch ####
-cluster = list(
-  ICL7 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
-  ICL7bis = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
-  EZL1 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
-  EZL1bis = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2))
-)
-
-countdata = read.table(paste0("./DATA/Pour_DESeq/",condition,"_expression_table_pour_DESeq_v2.tab"), sep="\t",row.names=1,header =  T)
-
-# Création du tableau avec les info des colonnes
-infodata = CreatInfoData3(countdata, conditions = condition , rnai_list, cluster, Timing = time)
-
-
-# Creation de l'objet DESeq2
-countdata = as.matrix(countdata)
-deseq = DESeqDataSetFromMatrix(countData = countdata,
-                               colData  = infodata,
-                               design   = ~ Condition)
-
-
-# Analyse DESeq2
-print(paste(condition , "-----> Analyse DESeq2"))
-deseq = DESeq(deseq)
-
-# Récupération des données de comptage normalisées
-data_tab = counts(deseq,normalized=T)
-
-##### Analyse multi-variée des données pour clustering  #####
-
-# Analyse en composante principale
-print(paste( condition, "-----> Analyse ACP"))
-PCA_plot_generator(data_tab,colors = seq_color,
-                   save_path = paste0(path,"/AVEC_ComBat/"),
-                   main = paste0("ACP ", condition," (DESeq2)"),
-                   sortie = "png")
-
-
-# Analyse en composante principale
-print(paste( condition, "-----> Analyse ACP"))
-PCA_plot_generator(data_tab,colors = clust_color,
-                   save_path = paste0(path,"/AVEC_ComBat_cluster/"),
-                   main = paste0("ACP ", condition," (DESeq2)"),
-                   sortie = "png")
-
-
-print(paste( condition, "-----> Clustering en cours"))
-dir.create(paste0(path,"AVEC_ComBat/4Cluster/"),recursive=T,showWarnings=F)
-for (distance in c("Pearson", "Spearman")){
-  png(paste0(path,"AVEC_ComBat/4Cluster/", condition,"_Matrice_",distance,".png"),  width = 600, height = 600)
-  # Choisir le mode de calcule des distances
-  if (distance == "Pearson"){
-    matDist = as.matrix(cor(data_tab))
-    p= pheatmap(matDist, main = paste("Pheatmap Pearson DESeq2",  condition), cluster_rows = F, cluster_cols = F)
-    print(p)
-    matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
-    
-  }else if (distance == "Spearman"){
-    matDist = as.matrix(cor(data_tab,method="spearman"))
-    p= pheatmap(matDist, main = paste("Pheatmap Spearman DESeq2",  condition), cluster_rows = F, cluster_cols = F)
-    print(p)
-    matDist = as.dist(1-cor(log2(data_tab+1), method="spearman"))
-  }
-  dev.off()
+if (condition == names(rnai_list)[2]){
+  cluster = list(
+    ICL7 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
+    ICL7bis = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
+    EZL1 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
+    EZL1bis = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2))
+  )
+}
+for (version in 1:2){
+  countdata = read.table(paste0("./DATA/Pour_DESeq/",condition,"_expression_table_pour_DESeq_v",version,".tab"), sep="\t",row.names=1,header =  T)
+  
+  # Création du tableau avec les info des colonnes
+  infodata = CreatInfoData3(countdata, conditions = condition , rnai_list, cluster, Timing = time)
   
   
-  for (method in c("kmeans", "HCL")){
-    print(paste(distance, method))
-    png(paste0(path,"AVEC_ComBat/4Cluster/", condition,"_Cluster_",method,"_",distance,".png"),  width = 800, height = 600)
-    Clustering(matDist = matDist,
-               nb_cluster = 5,
-               method = method,
-               titre = paste("DESeq2", condition),
-               colors = color)
+  # Creation de l'objet DESeq2
+  countdata = as.matrix(countdata)
+  deseq = DESeqDataSetFromMatrix(countData = countdata,
+                                 colData  = infodata,
+                                 design   = ~ Condition)
+  
+  
+  # Analyse DESeq2
+  print(paste(condition , "-----> Analyse DESeq2"))
+  deseq = DESeq(deseq)
+  
+  # Récupération des données de comptage normalisées
+  data_tab = counts(deseq,normalized=T)
+  
+  ##### Analyse multi-variée des données pour clustering  #####
+  
+  # Analyse en composante principale
+  print(paste( condition, "-----> Analyse ACP"))
+  PCA_plot_generator(data_tab,colors = seq_color,
+                     save_path = paste0(path,"/AVEC_ComBatv",version,"/"),
+                     main = paste0("ACP ", condition," (DESeq2)"),
+                     sortie = "png")
+  
+  
+  # Analyse en composante principale
+  print(paste( condition, "-----> Analyse ACP"))
+  PCA_plot_generator(data_tab,colors = clust_color,
+                     save_path = paste0(path,"/AVEC_ComBatv",version,"_cluster/"),
+                     main = paste0("ACP ", condition," (DESeq2)"),
+                     sortie = "png")
+  
+  
+  print(paste( condition, "-----> Clustering en cours"))
+  dir.create(paste0(path,"AVEC_ComBatv",version,"/4Cluster/"),recursive=T,showWarnings=F)
+  for (distance in c("Pearson", "Spearman")){
+    png(paste0(path,"AVEC_ComBatv",version,"/4Cluster/", condition,"_Matrice_",distance,".png"),  width = 600, height = 600)
+    # Choisir le mode de calcule des distances
+    if (distance == "Pearson"){
+      matDist = as.matrix(cor(data_tab))
+      p= pheatmap(matDist, main = paste("Pheatmap Pearson DESeq2",  condition), cluster_rows = F, cluster_cols = F)
+      print(p)
+      matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
+      
+    }else if (distance == "Spearman"){
+      matDist = as.matrix(cor(data_tab,method="spearman"))
+      p= pheatmap(matDist, main = paste("Pheatmap Spearman DESeq2",  condition), cluster_rows = F, cluster_cols = F)
+      print(p)
+      matDist = as.dist(1-cor(log2(data_tab+1), method="spearman"))
+    }
     dev.off()
+    
+    
+    for (method in c("kmeans", "HCL")){
+      print(paste(distance, method))
+      png(paste0(path,"AVEC_ComBatv",version,"/4Cluster/", condition,"_Cluster_",method,"_",distance,".png"),  width = 800, height = 600)
+      Clustering(matDist = matDist,
+                 nb_cluster = 5,
+                 method = method,
+                 titre = paste("DESeq2", condition),
+                 colors = color)
+      dev.off()
+    }
   }
 }
 
-#### Seq 2014 ####
-
-condition = names(rnai_list)[3]
-
-print(paste("On analyse le jeu de donnee :", condition , "-->", paste(rnai_list[[condition]], collapse = ", ") ))
-
-path = paste0(path_dir,condition ,"/")
-dir.create(path,recursive=T,showWarnings=F)
-
-##### Analyse DESeq2 ####
-# Ouverture des fichiers countdata SANS correction de l'effet Batch
-countdata = read.table(paste0("./DATA/Pour_DESeq_SansCorrectionBatch/",condition ,"_expression_table_ROW.tab"), sep="\t",row.names=1,header =  T)
-
-# Création du tableau avec les info des colonnes
-infodata = CreatInfoData3(countdata, conditions = condition , rnai_list, cluster)
-
-# Creataion de l'objet DESeq2
-countdata = as.matrix(countdata)
-deseq = DESeqDataSetFromMatrix(countData = countdata,
-                               colData  = infodata,
-                               design   = ~ Condition)
-
-
-# Analyse DESeq2
-print(paste(condition , "-----> Analyse DESeq2"))
-deseq = DESeq(deseq)
-
-# Récupération des données de comptage normalisées
-data_tab = counts(deseq,normalized=T)
-
-##### Analyse multi-variée des données pour clustering  #####
-# Créaction du vecteur de couleur par cluster
-color = colnames(data_tab)
-for (j in rnai_list[[condition]]){
-  color[grep(paste0(j,"_"), color)]=seq_color[[j]]
+if (condition == names(rnai_list)[2]){
+  #### Seq 2014 ####
+  
+  condition = names(rnai_list)[3]
+  
+  print(paste("On analyse le jeu de donnee :", condition , "-->", paste(rnai_list[[condition]], collapse = ", ") ))
+  
+  path = paste0(path_dir,condition ,"/")
+  dir.create(path,recursive=T,showWarnings=F)
+  
+  ##### Analyse DESeq2 ####
+  # Ouverture des fichiers countdata SANS correction de l'effet Batch
+  countdata = read.table(paste0("./DATA/Pour_DESeq_SansCorrectionBatch/",condition ,"_expression_table_ROW.tab"), sep="\t",row.names=1,header =  T)
+  
+  # Création du tableau avec les info des colonnes
+  infodata = CreatInfoData3(countdata, conditions = condition , rnai_list, cluster)
+  
+  # Creataion de l'objet DESeq2
+  countdata = as.matrix(countdata)
+  deseq = DESeqDataSetFromMatrix(countData = countdata,
+                                 colData  = infodata,
+                                 design   = ~ Condition)
+  
+  
+  # Analyse DESeq2
+  print(paste(condition , "-----> Analyse DESeq2"))
+  deseq = DESeq(deseq)
+  
+  # Récupération des données de comptage normalisées
+  data_tab = counts(deseq,normalized=T)
+  
+  ##### Analyse multi-variée des données pour clustering  #####
+  # Créaction du vecteur de couleur par cluster
+  color = colnames(data_tab)
+  for (j in rnai_list[[condition]]){
+    color[grep(paste0(j,"_"), color)]=seq_color[[j]]
+  }
+  
+  # Analyse en composante principale
+  print(paste( condition, "-----> Analyse ACP"))
+  PCA_plot_generator(data_tab,colors = color,
+                     save_path = paste0(path,"/SANS_ComBat2014"),
+                     main = paste0("ACP ", condition," (DESeq2)"),
+                     sortie = "png")
+  
+  # Créaction du vecteur de couleur par cluster
+  color = colnames(data_tab)
+  for (j in rnai_list[[condition]]){
+    color[grep(paste0(j,"_"), color)]=cluster_color[[j]]
+  }
+  
+  # Analyse en composante principale
+  print(paste( condition, "-----> Analyse ACP"))
+  PCA_plot_generator(data_tab,colors = color,
+                     save_path = paste0(path,"/SANS_ComBat_cluster2014"),
+                     main = paste0("ACP ", condition," (DESeq2)"),
+                     sortie = "png")
+  
+  
+  #### SEquencer en 2020 ####
+  
+  condition = names(rnai_list)[4]
+  
+  print(paste("On analyse le jeu de donnee :", condition , "-->", paste(rnai_list[[condition]], collapse = ", ") ))
+  
+  path = paste0(path_dir,condition ,"/")
+  dir.create(path,recursive=T,showWarnings=F)
+  
+  ##### Analyse DESeq2 ####
+  # Ouverture des fichiers countdata SANS correction de l'effet Batch
+  countdata = read.table(paste0("./DATA/Pour_DESeq_SansCorrectionBatch/",condition ,"_expression_table_ROW.tab"), sep="\t",row.names=1,header =  T)
+  
+  # Création du tableau avec les info des colonnes
+  infodata = CreatInfoData3(countdata, conditions = condition , rnai_list, cluster)
+  
+  # Creataion de l'objet DESeq2
+  countdata = as.matrix(countdata)
+  deseq = DESeqDataSetFromMatrix(countData = countdata,
+                                 colData  = infodata,
+                                 design   = ~ Condition)
+  
+  
+  # Analyse DESeq2
+  print(paste(condition , "-----> Analyse DESeq2"))
+  deseq = DESeq(deseq)
+  
+  # Récupération des données de comptage normalisées
+  data_tab = counts(deseq,normalized=T)
+  
+  ##### Analyse multi-variée des données pour clustering  #####
+  # Créaction du vecteur de couleur par cluster
+  color = colnames(data_tab)
+  for (j in rnai_list[[condition]]){
+    color[grep(paste0(j,"_"), color)]=seq_color[[j]]
+  }
+  
+  # Analyse en composante principale
+  print(paste( condition, "-----> Analyse ACP"))
+  PCA_plot_generator(data_tab,colors = color,
+                     save_path = paste0(path,"/SANS_ComBat2020"),
+                     main = paste0("ACP ", condition," (DESeq2)"),
+                     sortie = "png")
+  
+  # Créaction du vecteur de couleur par cluster
+  color = colnames(data_tab)
+  for (j in rnai_list[[condition]]){
+    color[grep(paste0(j,"_"), color)]=cluster_color[[j]]
+  }
+  
+  # Analyse en composante principale
+  print(paste( condition, "-----> Analyse ACP"))
+  PCA_plot_generator(data_tab,colors = color,
+                     save_path = paste0(path,"/SANS_ComBat_cluster2020"),
+                     main = paste0("ACP ", condition," (DESeq2)"),
+                     sortie = "png")
+  
+  
 }
-
-# Analyse en composante principale
-print(paste( condition, "-----> Analyse ACP"))
-PCA_plot_generator(data_tab,colors = color,
-                   save_path = paste0(path,"/SANS_ComBat2014"),
-                   main = paste0("ACP ", condition," (DESeq2)"),
-                   sortie = "png")
-
-# Créaction du vecteur de couleur par cluster
-color = colnames(data_tab)
-for (j in rnai_list[[condition]]){
-  color[grep(paste0(j,"_"), color)]=cluster_color[[j]]
-}
-
-# Analyse en composante principale
-print(paste( condition, "-----> Analyse ACP"))
-PCA_plot_generator(data_tab,colors = color,
-                   save_path = paste0(path,"/SANS_ComBat_cluster2014"),
-                   main = paste0("ACP ", condition," (DESeq2)"),
-                   sortie = "png")
-
-
-#### SEquencer en 2020 ####
-
-condition = names(rnai_list)[4]
-
-print(paste("On analyse le jeu de donnee :", condition , "-->", paste(rnai_list[[condition]], collapse = ", ") ))
-
-path = paste0(path_dir,condition ,"/")
-dir.create(path,recursive=T,showWarnings=F)
-
-##### Analyse DESeq2 ####
-# Ouverture des fichiers countdata SANS correction de l'effet Batch
-countdata = read.table(paste0("./DATA/Pour_DESeq_SansCorrectionBatch/",condition ,"_expression_table_ROW.tab"), sep="\t",row.names=1,header =  T)
-
-# Création du tableau avec les info des colonnes
-infodata = CreatInfoData3(countdata, conditions = condition , rnai_list, cluster)
-
-# Creataion de l'objet DESeq2
-countdata = as.matrix(countdata)
-deseq = DESeqDataSetFromMatrix(countData = countdata,
-                               colData  = infodata,
-                               design   = ~ Condition)
-
-
-# Analyse DESeq2
-print(paste(condition , "-----> Analyse DESeq2"))
-deseq = DESeq(deseq)
-
-# Récupération des données de comptage normalisées
-data_tab = counts(deseq,normalized=T)
-
-##### Analyse multi-variée des données pour clustering  #####
-# Créaction du vecteur de couleur par cluster
-color = colnames(data_tab)
-for (j in rnai_list[[condition]]){
-  color[grep(paste0(j,"_"), color)]=seq_color[[j]]
-}
-
-# Analyse en composante principale
-print(paste( condition, "-----> Analyse ACP"))
-PCA_plot_generator(data_tab,colors = color,
-                   save_path = paste0(path,"/SANS_ComBat2020"),
-                   main = paste0("ACP ", condition," (DESeq2)"),
-                   sortie = "png")
-
-# Créaction du vecteur de couleur par cluster
-color = colnames(data_tab)
-for (j in rnai_list[[condition]]){
-  color[grep(paste0(j,"_"), color)]=cluster_color[[j]]
-}
-
-# Analyse en composante principale
-print(paste( condition, "-----> Analyse ACP"))
-PCA_plot_generator(data_tab,colors = color,
-                   save_path = paste0(path,"/SANS_ComBat_cluster2020"),
-                   main = paste0("ACP ", condition," (DESeq2)"),
-                   sortie = "png")
-
-
-

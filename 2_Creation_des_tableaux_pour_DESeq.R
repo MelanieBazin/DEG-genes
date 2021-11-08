c96cc0b56da551e478e2d4cec9ef9e523cb2f9b1
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2021-11-08 16:54:54 +0100

    considérer que les points séquancer en 2014 et 2020 pour la compraison

--- 2_Creation_des_tableaux_pour_DESeq.R
+++ 2_Creation_des_tableaux_pour_DESeq.R
@@ -9,31 +9,35 @@ set.seed(10111)
 dir.create("./DATA/Pour_DESeq/",recursive=T,showWarnings=F)
 dir.create("./DATA/Pour_DESeq_SansCorrectionBatch/",recursive=T,showWarnings=F)
 
-rnai_list = rnai_list[2:4]
 
-i = names(rnai_list)[1]
-# for (i in names(rnai_list)){
+i = names(rnai_list)[2]
+for (i in names(rnai_list)[1:2]){
   ##### Création du tableau de donnée à analyser ensemble ####
   #Ouverture des fichiers et création de l'objet countdata
   countdata = ConcatTab(type = "EXPRESSION", conditions = rnai_list[[i]])
-  write.table(countdata,paste0("./DATA/Pour_DESeq_SansCorrectionBatch/",i,"_expression_table_ROW.tab"), sep="\t",row.names=T,quote=F)
-  
+
   # Enregistrement des tableau qui seront utiliser pour DESeq
   infodata = CreatInfoData3(countdata, conditions = i , rnai_list, cluster)
   countdata = as.matrix(countdata)
   # Correction de l'effet batch avec ComBat
   if (i =="tout"){
+    write.table(countdata,paste0("./DATA/Pour_DESeq_SansCorrectionBatch/",i,"_expression_table_ROW.tab"), sep="\t",row.names=T,quote=F)
     batch = paste(infodata$Batch,infodata$Labo, sep = "_")
     countdata = ComBat_seq(countdata, batch = batch, group = infodata$Cluster)
   } else if(i == "seq2014vs2020"){
+    write.table(countdata,paste0("./DATA/Pour_DESeq_SansCorrectionBatch/",i,"_expression_table_ROW.tab"), sep="\t",row.names=T,quote=F)
+    timing = unique(infodata$Timing[which(infodata$Batch == "seq_2020")])
+    countdata = countdata[,is.element(infodata$Timing, timing)]
+    infodata = infodata[is.element(infodata$Timing, timing),]
     countdata = ComBat_seq(countdata, batch = infodata$Batch, group = infodata$Timing)
   } else if (length(grep("ICL7", colnames(countdata)))>0){
+    write.table(countdata,paste0("./DATA/Pour_DESeq_SansCorrectionBatch/",i,"_expression_table_ROW.tab"), sep="\t",row.names=T,quote=F)
     countdata = ComBat_seq(countdata, batch = infodata$Labo, group = infodata$Cluster)
   }
   write.table(countdata,paste0("./DATA/Pour_DESeq/",i,"_expression_table_pour_DESeq_v2.tab"), sep="\t",row.names=T,quote=F)
   
   print(paste("Tableau pour la condition",i, "termine"))
-# }
+}
 
 # Pour ouvrir les tableau avec correction BATch
 # countdata = read.table(paste0("./DATA/Pour_DESeq/",i,"_expression_table_pour_DESeq.tab"), sep="\t",row.names=1,header =  T)

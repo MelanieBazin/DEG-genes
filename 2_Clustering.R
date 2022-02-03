f9f3c82574155607bac04d28c771da43df1cab11
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2022-02-03 12:22:17 +0100

    Ajout des kmean + simplifucation

--- 2_Clustering.R
+++ 2_Clustering.R
@@ -87,72 +87,10 @@ res = kmeans(matDist, 4)
 png(paste0(path,"/Visualisation/Cluster/", condition,"_Kmean_pearson_vst.png"),  width = 800, height = 600)
 fviz_cluster(res, data = matDist, geom = c("point",  "text"), labelsize = 10, repel = T, 
                 show.clust.cent = F, ellipse = T, ggtheme = theme_bw(),
-                main = paste(method, "avec", nb_cluster, "cluster - distance :", distance,"\n", titre), 
+                main = "pearson_vst_kmean", 
                 xlab = "Principal Component 1",
                 ylab = "Principal Component 2")
 dev.off()
 
 # MyHeatmaps(path = paste0(path,"/Visualisation/Cluster/"),
 #            vsd, infodata, condition)
-
-#### Avec condition de groupe ####
-# Ouverture des fichiers avec correction de l'effet Batch avec la condition de groupe
-countdata = read.table(paste0("./DATA/Pour_DESeq/",condition ,"_expression_table_pour_DESeq_v2.tab"), sep="\t",row.names=1,header =  T)
-
-##### Analyse DESeq2 ####
-# Création du tableau avec les info des colonnes
-infodata = CreatInfoData3(countdata, conditions = condition , rnai_list, cluster)
-
-# Créataion de l'objet DESeq2
-countdata = as.matrix(countdata)
-deseq = DESeqDataSetFromMatrix(countData = countdata,
-                               colData  = infodata,
-                               design   = ~ Condition)
-
-# Definition des réplicats techniques
-deseq = collapseReplicates(deseq,
-                           groupby = deseq$Samples,
-                           run     = deseq$Names)
-
-color = c()
-for (r in colnames(deseq)){
-  clust = infodata[r, "Cluster"]
-  if (clust == "VEG"){
-    color = c(color, clust_color["veg"])
-  } else if(clust == "EARLY" ){
-    color = c(color, clust_color["early"])
-  } else if(clust == "INTER" ){
-    color = c(color, clust_color["inter"])
-  } else if(clust == "LATE" ){
-    color = c(color, clust_color["late"])
-  }
-}
-
-
-# Analyse DESeq2
-deseq = DESeq(deseq)
-
-### Recupération des donnée normalisée ####
-# Variance stabilizing transformation
-vsd = assay(vst(deseq, blind = T))
-write.table(vsd,paste0(path,condition ,"_expression_table_vst.tab"), sep="\t",row.names=T,quote=F)
-
-PCA_plot_generator(vsd,
-                   colors = color,
-                   save_path = paste0(path,"Visualisation/ACP/vst_avcGRP/"),
-                   main = paste0("ACP ", condition," (vst)"),
-                   sortie = "png")
-
-matDist = as.dist(1-cor(log2(vsd+1), method="pearson"))
-res = hclust(matDist)
-res = as.dendrogram(res)
-labels_colors(res)= as.character(color)[order.dendrogram(res)]
-
-png(paste0(path,"/Visualisation/Cluster/", condition,"_Cluster_grp_pearson_vst.png"),  width = 800, height = 600)
-plot(res, main = "pearson_vst")
-dev.off()
-
-vsd_mean = MeanTabCalculation(vsd, infodata)
-
-# MyHeatmaps(path = paste0(path,"/Visualisation/Cluster/"),
-#            vsd_mean, infodata, condition, moyenne = T)

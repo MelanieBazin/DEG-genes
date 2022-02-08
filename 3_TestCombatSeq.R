48dc681afaf59d18c57bc201fc199f0cd6356178
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2022-02-08 10:44:19 +0100

    Simplifier le code pour les couleur

--- 3_TestCombatSeq.R
+++ 3_TestCombatSeq.R
@@ -3,6 +3,7 @@ library(sva)
 library(DESeq2)
 library(pheatmap)
 library(MASS)
+library(dendextend)
 
 set.seed(10111)
 
@@ -22,21 +23,21 @@ dir.create("./DATA/DESeq2/",recursive=T,showWarnings=F)
 
 condition = names(rnai_list["HiSeqvsNextSeq"])
 
-print(paste("On analyse le jeu de donnee :", condition , "-->", paste(rnai_list[[condition]], collapse = ", ") ))
+print(paste("Analysis of dataset :", condition , "-->", paste(rnai_list[[condition]], collapse = ", ") ))
 
 path = paste0(path_dir,condition ,"/")
 dir.create(path,recursive=T,showWarnings=F)
 
-##### Analyse DESeq2 ####
+
+correction = "corrected"
 for (correction in c("corrected","uncorrected")){
-  if (correction == "corrected"){
-    c = "AVEC"
-  }else if (correction == "uncorrected"){
-    c = "SANS"
-  }
-  dir.create(paste0(path,"/",c,"_ComBat/"),recursive=T,showWarnings=F)
-  print(paste("Teste de conditions",c,"correction ComBatSeq"))
+  print(paste("Test of conditions",correction, "by ComBatSeq"))
   
+  path = paste0(path,"/",correction,"/")
+  dir.create(path,recursive=T,showWarnings=F)
+  
+  #### Analyse DESeq2 ####
+  # Ouerture du tableau de donnée
   countdata = read.table(paste0("./DATA/Pour_DESeq/",condition ,"_expression_table_",correction,".tab"), sep="\t",row.names=1,header =  T)
   
   # Création du tableau avec les info des colonnes
@@ -58,29 +59,17 @@ for (correction in c("corrected","uncorrected")){
   
   
   # Analyse DESeq2
-  print(paste(condition , "-----> Analyse DESeq2"))
+  print(paste(condition , "-----> DESeq2 analysis"))
   deseq = DESeq(deseq)
   
   # Récupération des données de comptage normalisées
   data_tab = assay(vst(deseq, blind = T))
-  write.table(data_tab,paste0("./DATA/DESeq2/",condition,"_expression_table_",correction,".tab"), sep="\t",row.names=T,quote=F)
   
-  ##### Analyse multi-variée des données pour clustering  #####
-  print(paste( condition, "-----> PCA Analyse"))
-  # Créaction du vecteur de couleur par année de sequancage
-  seq_color = Batch_color(condition, collapse = T)
-  names(seq_color) = colnames(data_tab)
+  write.table(data_tab,paste0(path,condition,"_expression_table_",correction,".tab"), sep="\t",row.names=T,quote=F)
+  write.table(infodata,paste0(path,condition,"_infodata_",correction,".tab"), sep="\t",row.names=T,quote=F)
   
-  print(seq_color)
-  
-  # Analyse en composante principale
-  PCA_plot_generator(data_tab,
-                     colors = seq_color,
-                     save_path = paste0(path,"/",c,"_ComBat/PCA/"),
-                     main = paste0("PCA ", condition," (DESeq2)"),
-                     sortie = "png")
-  
-  # Créaction du vecteur de couleur par cluster
+  #### Créer les vecteurs de couleurs  ####
+  # Créaction du vecteur de couleur 
   if (condition == names(rnai_list["HiSeqvsNextSeq"])){
     cluster2 = list(
       ICL7 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
@@ -88,60 +77,53 @@ for (correction in c("corrected","uncorrected")){
       EZL1 = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2)),
       EZL1bis = c(rep("EARLY",1),rep("INTER",1),rep("LATE",2))
     )
+  }else{
+    cluster2 = cluster
   }
   
-  clust_color = Culster_color(condition, cluster_list = cluster2)
-  names(clust_color) = colnames(data_tab)
-  
-  print(clust_color)
-  
-  # Analyse en composante principale
-  PCA_plot_generator(data_tab,
-                     colors = clust_color,
-                     save_path = paste0(path,"/",c,"_ComBat_cluster/PCA/"),
-                     main = paste0("PCA ", condition," (DESeq2)"),
-                     sortie = "png")
-  
-  print(paste( condition, "-----> Clustering hiérachique en cours"))
-  dir.create(paste0(path,c,"_ComBat/Cluster/"),recursive=T,showWarnings=F)
-  for (distance in c("Pearson", "Spearman")){
-    
-    # Choisir le mode de calcule des distances
-    if (distance == "Pearson"){
-      matDist = as.matrix(cor(data_tab))
-      p= pheatmap(matDist, main = paste("Pheatmap Pearson DESeq2",  condition), cluster_rows = F, cluster_cols = F)
-      matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
-      
-    }else if (distance == "Spearman"){
-      matDist = as.matrix(cor(data_tab,method="spearman"))
-      p= pheatmap(matDist, main = paste("Pheatmap Spearman DESeq2",  condition), cluster_rows = F, cluster_cols = F)
-      matDist = as.dist(1-cor(log2(data_tab+1), method="spearman"))
+  # color_type = "methods"
+  for (color_type in c("methods","replicates")){
+    # Choix de la couleur utilisé
+    print(paste( condition, "-----> Setting",color_type,"colors"))
+    if (color_type == "methods"){
+      # Créaction du vecteur de couleur par méthode de séquencage
+      color = Batch_color(condition, cluster_list = cluster2)
+    }else if (color_type == "replicates"){
+      # Créaction du vecteur de couleur par groupe de pseudo_réplicat
+      color = Culster_color(condition, cluster_list = cluster2)
     }
+    names(color) = colnames(data_tab)
     
-    png(paste0(path,c,"_ComBat/Cluster/", condition,"_Matrice_",distance,".png"),  width = 600, height = 600)
+    #### Analyse multi-variée des données pour clustering  ####
+    print(paste( condition, "-----> PCA analysis :", color_type))
+    PCA_plot_generator(data_tab,
+                       colors = color,
+                       save_path = paste0(path,"PCA_",color_type,"/"),
+                       main = paste0("PCA ", condition," (DESeq2)"),
+                       sortie = "png")
+    
+    
+    #### Matrice de distance et clusterng hiérarchique  ####
+    print(paste( condition, "-----> Hierarchical clustering :", color_type))
+    
+    matDist = as.matrix(cor(data_tab))
+    p= pheatmap(matDist, main = paste("Pheatmap Pearson",  condition), cluster_rows = F, cluster_cols = F)
+    
+    png(paste0(path, condition,"_Matrice_pearson.png"),  width = 600, height = 600)
     print(p)
     dev.off()
     
+    matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
+    res = hclust(matDist)
+    res = as.dendrogram(res)
+    labels_colors(res)= as.character(color)[order.dendrogram(res)]
     
-    for (method in c("kmeans", "HCL")){
-      print(paste(distance, method))
-      png(paste0(path,c,"_ComBat/Cluster/", condition,"_Cluster_",method,"_",distance,"_clutercolor.png"),  width = 800, height = 600)
-      Clustering(matDist = matDist,
-                 nb_cluster = 4,
-                 method = method,
-                 titre = paste("DESeq2", condition),
-                 colors = clust_color)
-      dev.off()
-      
-      png(paste0(path,c,"_ComBat/Cluster/", condition,"_Cluster_",method,"_",distance,"_methodcolor.png"),  width = 800, height = 600)
-      Clustering(matDist = matDist,
-                 nb_cluster = 4,
-                 method = method,
-                 titre = paste("DESeq2", condition),
-                 colors = seq_color)
-      dev.off()
-    }
+    png(paste0(path, condition,"_HCL_",color_type,".png"),  width = 800, height = 600)
+    plot(res, main = "pearson_vst")
+    dev.off()
   }
+  
+  
 }
 
 
@@ -149,34 +131,24 @@ if (condition == names(rnai_list["HiSeqvsNextSeq"])){
   print("Correlation matrix")
   #### Martice de corrélation ####
   
-  sansBatch = read.table(paste0("./DATA/DESeq2/",condition,"_expression_table_uncorrected.tab"), sep="\t",row.names=1,header =  T)
+  sansBatch = read.table(paste0(path_dir,condition,"/uncorrected/",condition,"_expression_table_uncorrected.tab"), sep="\t",row.names=1,header =  T)
   
-  avecBatch = read.table(paste0("./DATA/DESeq2/",condition,"_expression_table_corrected",".tab"), sep="\t",row.names=1,header =  T)
+  avecBatch = read.table(paste0(path_dir,condition,"/corrected/",condition,"_expression_table_corrected",".tab"), sep="\t",row.names=1,header =  T)
   colnames(avecBatch) = paste0(colnames(avecBatch),"_corrected")
   
   data_tab = cbind(sansBatch, avecBatch)
   data_tab = data_tab[,c(grep("EZL1", colnames(data_tab)),grep("ICL7", colnames(data_tab)))]
   
-  dir.create(paste0(path,"AVEC_ComBat"),recursive=T,showWarnings=F)
-  for (distance in c("Pearson", "Spearman")){
-    
-    # Choisir le mode de calcule des distances
-    if (distance == "Pearson"){
-      matDist = as.matrix(cor(data_tab))
-      p= pheatmap(matDist, main = paste("Pheatmap Pearson DESeq2",  condition), cluster_rows = F, cluster_cols = F)
-      matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
-      
-    }else if (distance == "Spearman"){
-      matDist = as.matrix(cor(data_tab,method="spearman"))
-      p= pheatmap(matDist, main = paste("Pheatmap Spearman DESeq2",  condition), cluster_rows = F, cluster_cols = F)
-      matDist = as.dist(1-cor(log2(data_tab+1), method="spearman"))
-    }
-    
-    png(paste0(path,"AVEC_ComBat/", condition,"_Matrice_",distance,".png"),  width = 600, height = 600)
-    print(p)
-    dev.off()
-  }
   
+  # Choisir le mode de calcule des distances
+  
+  matDist = as.matrix(cor(data_tab))
+  p= pheatmap(matDist, main = paste("Pheatmap Pearson DESeq2",  condition), cluster_rows = F, cluster_cols = F)
+  matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
+  
+  png(paste0(path_dir,condition,"/" , condition,"_Matrix.png"),  width = 600, height = 600)
+  print(p)
+  dev.off()
   
 } 
 

da7bac690e2aca2e6ea66be05dcffa68d1c29b9b
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2022-02-14 09:43:35 +0100

    Modification des filtres

--- 5-1_Filtres.R
+++ 5-1_Filtres.R
@@ -1,7 +1,20 @@
-all_genes = annotation$ID
-inter_genes = annotation$ID[annotation$EXPRESSION_PROFIL == "Intermediate peak"]
-early_genes = annotation$ID[annotation$EXPRESSION_PROFIL == "Early peak"]
-auto_genes = annotation$ID[annotation$EXPRESSION_PROFIL != "none"]
+#♪ Récupéartion des données annotation
+annotation = read.table("./DATA/My_annotation2.tab",header=T,sep="\t")
+annotation = annotation[,c(1,3:5,13,6:11,2)]
+rownames(annotation)=annotation$ID
+
+AUTOGAMY = list(all_genes = annotation$ID,
+                 autogamy = annotation$ID[annotation$EXPRESSION_PROFIL != "none"],
+                 inter_peak = annotation$ID[annotation$EXPRESSION_PROFIL == "Intermediate peak"],
+                 early_peak = annotation$ID[annotation$EXPRESSION_PROFIL == "Early peak"])
+
+# Récupération des données tuboID
+TurboPGM = read.table("./DATA/TurboID/2114003-Pgm-ProteinMeasurements.txt",header=T,sep="\t")
+TurboPGML4 = read.table("./DATA/TurboID/2114003-PgmL4-ProteinMeasurements.txt",header=T,sep="\t",quote='')
+
+TURBO = list(turbo_PGM = annotation$ID[which(is.element(annotation$PROTEIN_NAME, TurboPGM$PROTEIN_NAME))] ,
+             turbo_PGML4 = annotation$ID[which(is.element(annotation$PROTEIN_NAME, TurboPGML4$PROTEIN_NAME))],
+             turbo_OU = annotation$ID[which(is.element(annotation$PROTEIN_NAME, unique(TurboPGM$PROTEIN_NAME, TurboPGML4$PROTEIN_NAME)))])
 
 #### Ouverture tableau de données des gènes DEG ####
 path = paste0("./Analyse/",file_name, "/", condition,"/")
@@ -32,37 +45,35 @@ up_pkx = intersect(UP_PKX[["UP_XRCC4"]], intersect(UP_PKX[["UP_PGM"]],UP_PKX[["U
 not_UP_PKX = list()
 for (l in names(UP_PKX)){
   up_genes = UP_PKX[[l]]
-  not_UP_PKX = c(not_UP_PKX, list(setdiff(all_genes, up_genes)))
+  not_UP_PKX = c(not_UP_PKX, list(all_genes[!is.element(all_genes, up_genes)]))
 }
 names(not_UP_PKX) = paste("not",names(UP_PKX), sep = "_")
 
-# Les gènes DOWN dérégulés en PGM, KU80c et/OU XRCC4
-DOWN_PKX = list()
-for (R in RNAi[-1]){
-  tab = TAB[[R]]
-  tab = tab[tab$REGULATION == "Down-regulated", "ID"]
-  DOWN_PKX = c(DOWN_PKX, list(tab))
-}
-names(DOWN_PKX) = paste("DOWN", RNAi[-1], sep = "_")
 
-# Les gènes NOT dérégulés en PGM, KU80c et/OU XRCC4
-not_DE_PKX = list()
-for (l in names(UP_PKX)){
-  up_genes = UP_PKX[[l]]
-  down_genes = DOWN_PKX[[l]]
-  not_DE_PKX = c(not_DE_PKX, list(setdiff(all_genes, unique(down_genes,up_genes))))
-}
-names(not_DE_PKX) = paste("not_DE",RNAi[-1], sep = "_")
 
 # Les gènes DOWN dérégulés en CTIP early ou inter
 CTIP = rbind(TAB[["CTIP_early"]],TAB[["CTIP_inter"]])
 
-DOWN_C = unique(CTIP[CTIP$REGULATION == "Down-regulated", "ID"])
-up_pkx_down_c = intersect(up_pkx, DOWN_C)
+stdCTIP = list(DOWN_CTIP = unique(CTIP[CTIP$REGULATION == "Down-regulated", "ID"]),
+               UP_ALL = up_pkx,
+               DOWN_UP = intersect(DOWN_C, up_pkx))
 
-# Les gènes UP dérégulés en CTIP
-UP_C = unique(CTIP[CTIP$REGULATION == "Up-regulated", "ID"])
 
-# Les gènes NOT DE dérégulés en CTIP
-not_DE_C = setdiff(all_genes, unique(DOWN_C,UP_C))
+not_stdCTIP = list(not_DOWN_CTIP = not_DOWN_C,
+                   not_UP_ALL = not_UP_PKX,
+                   not_DOWN_UP = intersect(not_DOWN_C, not_UP_PKX))
+
+Crossinglist <- function (list1, list2){
+  LIST = list()
+  list_name = c()
+  for (l in names(list1)){
+    for (m in names(list2)){
+      LIST = c(LIST, list(intersect(list1[[l]],list2[[m]])))
+      list_name = c(list_name, paste(names(list1[l]),names(list2[m]), sep = "x"))
+    }
+  }
+  names(LIST)=list_name
+  
+  return(LIST)
+}
 

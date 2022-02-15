2a49d40cb4717223972bdc063cddc77df219e27f
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2022-02-15 18:32:43 +0100

    modification des IF pour la list ARNi

--- 4_Analyse.R
+++ 4_Analyse.R
@@ -61,13 +61,13 @@ for (condition in names(rnai_list)){
   dev.off()
   
   ##### Analyse DESeq2 ####
-    
+  
   # Ouverture des fichiers countdata avec correction de l'effet Batch sans les groupe
   countdata = read.table(paste0("./DATA/Pour_DESeq/",condition ,"_expression_table_corrected.tab"), sep="\t",row.names=1,header =  T)
   
   # Création du tableau avec les info des colonnes
   infodata = CreatInfoData(countdata, conditions = condition , rnai_list, cluster)
-
+  
   # Créataion de l'objet DESeq2
   countdata = as.matrix(countdata)
   deseq = DESeqDataSetFromMatrix(countData = countdata,
@@ -85,33 +85,35 @@ for (condition in names(rnai_list)){
   # Analyse DESeq2
   print(paste(condition , "-----> Analyse DESeq2"))
   deseq = DESeq(deseq)
-
+  
   # Graphique du paramètre de dispersion
   # pdf(paste0(path,condition ,"_disperssion_DESeq2.pdf"))
   png(paste0(path_dir,condition ,"/Visualisation/Disperssion_DESeq2.png"))
   plotDispEsts(deseq, ylim = c(1e-6, 1e1))
   dev.off()
-
+  
   # Récupération des données de comptage normalisées
   # data_tab = counts(deseq,normalized=T)
   data_tab = assay(vst(deseq, blind = T))
   
   write.table(data_tab,paste0(path,condition ,"_expression_table_vst.tab"), sep="\t",row.names=T,quote=F)
   write.table(infodata,paste0(path,condition ,"_infodata.tab"), sep="\t",row.names=T,quote=F)
-
+  
   #### Lancer l'analyse de gènes dérégulés ####
-
+  
   # Definir les condition à analyser
   RNAi_list = unique(rnai_list[[condition ]])
+  if (any(grepl("bis", RNAi_list))){
   RNAi_list = RNAi_list[-grep("bis", RNAi_list)]
-  if (is.element("ICL7", RNAi_list)){
+  }
+  if (any(grepl("ICL7", RNAi_list))){
     RNAi_list = RNAi_list[-grep("ICL7",RNAi_list)]
   }
-  if (!is.null(grep("ND7", RNAi_list))){
+  if (any(grepl("ND7", RNAi_list))){
     RNAi_list = RNAi_list[-grep("ND7",RNAi_list)]
   }
   
-  # Regarder la derégulation dnas chaque condition
+  # Regarder la derégulation dans chaque condition
   for (RNAi in RNAi_list){
     print(paste(condition, ": Analyse des donnees pour  ----->", RNAi ))
     
@@ -135,12 +137,20 @@ for (condition in names(rnai_list)){
   path = paste0(path_dir,condition ,"/Visualisation/")
   
   source("4-2_Visualisation_des_donnees.R")
-
+  
   print(paste("Visualisation des donnee fini pour", condition ))
+  
+  
+  if (condition == "HiSeqvsNextSeq"){
+    path = paste0(path_dir,condition,"/")
+    
+    # comparaison avec les donnée publiées pour EZL1
+    print("Comparaison avec les données publiée dans Frapporti et al. 2019")
+    source("4-3_Comparaison_Frapporti.R")
+  }
 }
 
-# comparaison avec les donnée publiées pour EZL1
-source("4-3_Comparaison_Frapporti.R")
+
 
 sink(paste0("./Analyse/",analyseName,"/sessionInfo.txt"))
 print(sessionInfo())

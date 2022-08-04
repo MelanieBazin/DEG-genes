ccd571b79ad4a81a8d92a04e57c5188e9dead419
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2022-08-04 15:20:22 +0200

    ajour boxplot pour profils d'expression

--- 4-2_Visualisation_des_donnees.R
+++ 4-2_Visualisation_des_donnees.R
@@ -34,7 +34,9 @@ ExpressionProfils(type = "vst",
                   file = path_dir,
                   select_ID = select_ID)
 
+#### Analyse multi-variée des données pour clustering  ####
 for (color_type in c("methods","replicates")){
+  
   # Choix de la couleur utilisé
   print(paste( condition, "-----> Setting",color_type,"colors"))
   if (color_type == "methods"){
@@ -45,34 +47,38 @@ for (color_type in c("methods","replicates")){
     color = Culster_color_info(data_tab,infodata)
   }
   
-  #### Analyse multi-variée des données pour clustering  ####
+  # PCA
   print(paste( condition, "-----> PCA analysis :", color_type))
   PCA_plot_generator(data_tab,
                      colors = color,
                      police_seize = 2,
                      save_path = paste0(path,"PCA_",color_type,"/"),
                      main = paste0("PCA ", condition," (DESeq2)"),
-                     sortie = "pdf")
+                     sortie = "pdf",
+                     rename = T)
+  
   PCA_ggplot_generator(data_tab,
                      infodata,
+                     color_type,
                      police_seize = 2,
-                     save_path = paste0(path,"PCA_",color_type,"/ggplot/"),
+                     save_path = path,
                      main = paste0("PCA ", condition," (DESeq2)"),
-                     sortie = "pdf")
+                     sortie = "pdf",
+                     rename = T)
   
   #### Matrice de distance et clusterng hiérarchique  ####
   print(paste( condition, "-----> Hierarchical clustering :", color_type))
-  
+
   matDist = as.matrix(cor(data_tab))
   pdf(paste0(path, condition,"_Matrice_pearson.pdf"),  width = 600, height = 600)
   pheatmap(matDist, main = paste("Pheatmap Pearson",  condition), cluster_rows = F, cluster_cols = F)
   dev.off()
-  
+
   matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
   res = hclust(matDist)
   res = as.dendrogram(res)
   labels_colors(res)= as.character(color)[order.dendrogram(res)]
-  
+
   pdf(paste0(path, condition,"_HCL_",color_type,".pdf"),  width = 800, height = 200)
   plot(res, main = "pearson_vst")
   dev.off()
@@ -94,3 +100,53 @@ write.table(mean_data_tab,paste0(path,condition ,"_MEANexpression_table_vst.tab"
 MyHeatmaps(paste0(path,"Visualisation/"),mean_data_tab,infodata_collapse, condition,  moyenne = T)
 
 print ("Heatmap mean done")
+
+##### Boxplot ####
+print(paste( condition, "-----> Conception des boxplot"))
+
+
+rnai = unique(infodata_collapse$Feeding)
+rnai = rnai[c(2,3:length(rnai),1)]
+
+profils = unique(annotation$EXPRESSION_PROFIL)
+
+
+
+pdf(paste0(path,condition , "_Boxplot_ExpressionVST", ".pdf"), width = 20, height = 5)
+par(mfrow = c(1,length(rnai)))
+for (p in profils){
+  
+  data = mean_data_tab[annotation$ID[annotation$EXPRESSION_PROFIL == p],]
+
+  for (r in rnai){
+    
+    data_box = data[,grep(r,colnames(data))]
+    
+    mediane = apply(data_box, 2, median)
+    
+    plot(NULL, main = p, sub = r,
+         ylim = c(0, 20),
+         ylab = "Expression level (vst)",
+         xlim = c(0.5,4.5),
+         xlab = "",
+         xaxt = "n")
+    
+    if(r == "PGM"){
+      boxplot(data_box, add = T, boxwex =0.5, col = "white", outline = F,
+              names = str_remove_all(colnames(data_box), paste0("_",r)),
+              at = c(1, 3, 4))
+      lines(c(mediane[1], mean(c(mediane[1], mediane[2])), mediane[2:3]), col = profile_color[p], lwd = 4)
+    }else{
+      boxplot(data_box, add = T, boxwex =0.5, col = "white", outline = F,
+            names = str_remove_all(colnames(data_box), paste0("_",r)))
+      lines(mediane, col = profile_color[p], lwd = 4)
+    }
+  }
+}
+dev.off()
+
+
+
+
+
+

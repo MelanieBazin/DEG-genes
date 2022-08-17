2a0bee156acf62f0328753c7727351e6a29b05cd
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2022-08-17 18:02:20 +0200

    Simplification + renommer

---
+++ 4-1_Data_visualization.R
@@ -0,0 +1,107 @@
+###################
+#### DESeq2 data visualization
+#### -> Boxplot of the normalized count
+#### -> PCA and hierarchical clustering
+###################
+options(stringsAsFactors = FALSE)
+
+#### Boxplot of normalized count ####
+print(paste("-----> Normalized counts boxplot"))
+pdf(paste0(graph_path, "Vst_Counts_bolxplot.pdf"))
+CountBoxplot(data_tab, "vst counts", color = c(rep("darkolivegreen2",28), rep("chartreuse4",21))) 
+dev.off()
+
+#### Cluster visualization (PCA and hclust)  ####
+for (color_type in c("methods","replicates")){
+  
+  # PCA analysis
+  print(paste("-----> PCA colored by :", color_type))
+  PCA_ggplot_generator(data_tab,
+                       infodata_collapse,
+                       color_type,
+                       police_seize = 4,
+                       point_seize = 2.5,
+                       save_path = graph_path,
+                       main = paste0("PCA ", condition," (DESeq2)"),
+                       sortie = "pdf",
+                       rename = T,
+                       w = 6.5,
+                       h = 6)
+  
+  # Define the color that will be used on the hierarchical clustering
+  cluster2 = rnai_list[[condition]][-grep("bis", rnai_list[[condition]] )]
+  cluster2 = cluster[which(is.element(names(cluster), cluster2))]
+  if (color_type == "methods"){
+    color = Batch_color(data_tab, cluster_list = cluster2) # Coloration by sequencing method
+  }else if (color_type == "replicates"){
+    color = Culster_color(data_tab, cluster_list = cluster2) # Coloration by autogamy stages
+  }
+  names(color) = colnames(data_tab)
+  
+  # Pearson correlation matrix & hierarchical clustering
+  print(paste("-----> Hierarchical clustering colored by :", color_type))
+  matDist = as.dist(1-cor(log2(data_tab+1), method="pearson"))
+  res = hclust(matDist)
+  res = as.dendrogram(res)
+  labels_colors(res)= as.character(color)[order.dendrogram(res)]
+  
+  pdf(paste0(graph_path, condition ,"_hclust_pearson_vst.pdf"),  width = 12, height = 2.5)
+  plot(res, main = "pearson_vst")
+  dev.off()
+}
+
+
+#### Boxplot of mean expression level ####
+print(paste("-----> Boxplot of expression level"))
+
+# Order the time course to plot
+rnai = unique(infodata_collapse$KnockDown)
+rnai = c(rnai[grep("ctrl", rnai)],rnai[-grep("ctrl", rnai)])
+
+profils = unique(annotation$EXPRESSION_PROFIL)
+
+# Calculate the mean expression of replicates
+mean_data_tab = MeanTabCalculation(data_tab, infodata_collapse)
+write.table(mean_data_tab,paste0(path,condition ,"_MEANexpression_table_vst.tab"), sep="\t",row.names=T,quote=F)
+
+pdf(paste0(graph_path,condition , "_Boxplot_ExpressionVST", ".pdf"), width = 20, height = 4)
+par(mfrow = c(1,length(rnai)))
+
+# For each expression profile
+for (p in profils){
+  data = mean_data_tab[annotation$ID[annotation$EXPRESSION_PROFIL == p],]
+  
+  # For each time course
+  for (r in rnai){
+    data_box = data[,grep(r,colnames(data))]
+    
+    mediane = apply(data_box, 2, median)
+    
+    # Create a boxplot + median of the expression profile
+    plot(NULL, main = p, sub = r,
+         ylim = c(0, 20),
+         ylab = "Expression level (vst)",
+         xlim = c(0.5,4.5),
+         xlab = "",
+         xaxt = "n")
+    
+    if(r == "PGM"){ # Exception for PGM since there is no EARLY stage
+      boxplot(data_box, add = T, boxwex =0.5, col = "white", outline = F,
+              names = str_remove_all(colnames(data_box), paste0("_",r)),
+              at = c(1, 3, 4))
+      lines(c(mediane[1], mean(c(mediane[1], mediane[2])), mediane[2:3]), col = profile_color[p], lwd = 4)
+    }else{
+      boxplot(data_box, add = T, boxwex =0.5, col = "white", outline = F,
+              names = str_remove_all(colnames(data_box), paste0("_",r)))
+      lines(mediane, col = profile_color[p], lwd = 4)
+    }
+  }
+}
+
+dev.off()
+
+
+
+
+
+

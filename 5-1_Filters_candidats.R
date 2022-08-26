c5967535a7bdb9e17c8550f33950fe8e55460adb
Melanie labo_fix <melanie.bazin@i2bc.paris-saclay.fr>
2022-08-26 10:31:35 +0200

    Cahnger filtre pour analyse ds profils d'expressions

--- 5-1_Filters_candidats.R
+++ 5-1_Filters_candidats.R
@@ -16,7 +16,7 @@ names(AUTOGAMY) = unique(annotation$EXPRESSION_PROFIL)
 # Loaclised the data
 path = paste0("./Analyse/",date,"_DESeq2_analysis/")
 path = paste0(path,
-              list.files(path)[grep(condition,list.files(path))],"/")
+              list.files(path)[grep(paste0(condition,"_FC-", FC, "_pval-", pvalue),list.files(path))],"/")
 
 # Definition of the time course that will be analysed
 infodata = read.table(paste0(path, condition,"_infodata_collapse.tab"))
@@ -35,7 +35,7 @@ for (R in RNAi){
 }
 names(DEtab_list)[3:length(names(DEtab_list))]= RNAi[-1]
 
-# UP deregulated genes
+# UP deregulated genes  in PGM, KU80c &XRCC4 KD
 UP_filter = list()
 for (R in RNAi[-1]){
   tab = DEtab_list[[R]]
@@ -44,24 +44,39 @@ for (R in RNAi[-1]){
 }
 rm(R, tab)
 names(UP_filter) = paste("UP", RNAi[-1], sep = "_")
-UP_filter = c(UP_filter, 
-              UP_PK =list(intersect(UP_filter$UP_PGM,
-                                    UP_filter$UP_KU80c)),
-              UP_PX =list(intersect(UP_filter$UP_PGM,
-                                    UP_filter$UP_XRCC4)),
-              UP_XK =list(intersect(UP_filter$UP_XRCC4,
-                                    UP_filter$UP_KU80c)),
+UP_filter = c(UP_filter,
               UP_PKX = list(intersect(UP_filter$UP_XRCC4, 
                                       intersect(UP_filter$UP_PGM,
                                                 UP_filter$UP_KU80c))))
+# DOWN regulated in PGM, KU80c &XRCC4 KD
+DOWN_pkx_filter = list()
+for (R in RNAi[-1]){
+  tab = DEtab_list[[R]]
+  tab = tab[tab$REGULATION == "Down-regulated", "ID"]
+  DOWN_pkx_filter  = c(DOWN_pkx_filter , list(tab))
+}
+rm(R, tab)
+names(DOWN_pkx_filter ) = paste("DOWN", RNAi[-1], sep = "_")
+DOWN_pkx_filter  = c(DOWN_pkx_filter ,
+                     DOWN_pkx_filter  = list(intersect(DOWN_pkx_filter$DOWN_XRCC4, 
+                                                       intersect(DOWN_pkx_filter$DOWN_PGM,
+                                                                 DOWN_pkx_filter$DOWN_KU80c))))
 
-# DOWN deregulated genes
-DOWN_filter = list(CTIP_early = DEtab_list[["CTIP_early"]][DEtab_list[["CTIP_early"]]$REGULATION == "Down-regulated", "ID"],
-                   CTIP_inter = DEtab_list[["CTIP_inter"]][DEtab_list[["CTIP_inter"]]$REGULATION == "Down-regulated", "ID"])
+# DOWN deregulated genes in CTIP KD
+DOWN_filter = list(D_CTIP_early = DEtab_list[["CTIP_early"]][DEtab_list[["CTIP_early"]]$REGULATION == "Down-regulated", "ID"],
+                   D_CTIP_inter = DEtab_list[["CTIP_inter"]][DEtab_list[["CTIP_inter"]]$REGULATION == "Down-regulated", "ID"])
 
 DOWN_filter = c(DOWN_filter, 
-                DOWN_CTIP = list(unique(c(DOWN_filter[["CTIP_early"]], 
-                                          DOWN_filter[["CTIP_inter"]]))))
+                DOWN_CTIP = list(unique(c(DOWN_filter[["D_CTIP_early"]], 
+                                          DOWN_filter[["D_CTIP_inter"]]))))
+
+# UP deregulated genes in CTIP KD
+UP_C_filter = list(UP_CTIP_early = DEtab_list[["CTIP_early"]][DEtab_list[["CTIP_early"]]$REGULATION == "Up-regulated", "ID"],
+                   UP_CTIP_inter = DEtab_list[["CTIP_inter"]][DEtab_list[["CTIP_inter"]]$REGULATION == "Up-regulated", "ID"])
+
+UP_C_filter = c(UP_C_filter, 
+                UP_CTIP = list(unique(c(UP_C_filter[["UP_CTIP_early"]], 
+                                        UP_C_filter[["UP_CTIP_inter"]]))))
 
 # Genes of interest
 DE_genes = list(UP_PKX = UP_filter$UP_PKX,
